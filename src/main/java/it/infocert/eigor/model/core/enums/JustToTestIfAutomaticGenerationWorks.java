070c5fad211cf941e6ae5eef99e216d273ab65af
danidemi <demichelis@danidemi.com>
2017-03-20 16:57:26 +0100

    Added a .java that references a generated enum, just for checking all works well.

---
+++ src/main/java/it/infocert/eigor/model/core/enums/JustToTestIfAutomaticGenerationWorks.java
@@ -0,0 +1,9 @@
+package it.infocert.eigor.model.core.enums;
+
+public class JustToTestIfAutomaticGenerationWorks {
+
+    public static void main(String[] args) {
+        System.out.println(Iso31661CountryCodes.CF);
+    }
+
+}
