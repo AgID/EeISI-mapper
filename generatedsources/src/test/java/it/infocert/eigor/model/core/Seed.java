651c88e58ae567f9a3179a95d9800804862d8bde
Matteo Joliveau <m.joliveau@anoki.it>
2017-03-27 13:08:53 +0200

    Finished refactoring

---
+++ generatedsources/src/test/java/it/infocert/eigor/model/core/Seed.java
@@ -0,0 +1,54 @@
+package it.infocert.eigor.model.core;
+
+import it.infocert.eigor.model.core.enums.Iso4217CurrenciesFundsCodes;
+import it.infocert.eigor.model.core.model.BT0001InvoiceNumber;
+import it.infocert.eigor.model.core.model.BT0006VatAccountingCurrencyCode;
+import it.infocert.eigor.model.core.model.CoreInvoice;
+import it.infocert.eigor.model.core.rules.Br002AnInvoiceShallHaveAnInvoiceNumberRule;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Seed {
+
+    @Test
+    public void shouldApplyRule() {
+
+        // given
+        Br002AnInvoiceShallHaveAnInvoiceNumberRule rule = new Br002AnInvoiceShallHaveAnInvoiceNumberRule();
+
+        CoreInvoice invoiceWithCoreInvoiceNumber = new CoreInvoice();
+        invoiceWithCoreInvoiceNumber.getBt0001InvoiceNumbers().add(new BT0001InvoiceNumber( "1234" ));
+
+        CoreInvoice invoiceWithoutCoreInvoiceNumber = new CoreInvoice();
+
+        // when
+        boolean outcome1 = rule.issCompliant(invoiceWithCoreInvoiceNumber);
+        boolean outcome2 = rule.issCompliant(invoiceWithoutCoreInvoiceNumber);
+
+        // then
+        assertThat( outcome1, is(true) );
+        assertThat( outcome2, is(false) );
+
+    }
+
+
+
+    @Test
+    public void justStart() {
+
+        // given
+        CoreInvoice coreInvoice = new CoreInvoice();
+        BT0001InvoiceNumber invoiceNumber = new BT0001InvoiceNumber( "1234" );
+
+        // when
+        coreInvoice.getBt0001InvoiceNumbers().add(invoiceNumber);
+        coreInvoice.getBt0006VatAccountingCurrencyCodes().add( new BT0006VatAccountingCurrencyCode(Iso4217CurrenciesFundsCodes.EUR) );
+
+        // then
+        assertThat( coreInvoice.getBt0001InvoiceNumbers().get(0), is(invoiceNumber) );
+
+    }
+
+}
---
+++ generatedsources/src/test/java/it/infocert/eigor/model/core/dump/DumpTest.java
@@ -0,0 +1,70 @@
+package it.infocert.eigor.model.core.dump;
+
+import it.infocert.eigor.model.core.enums.Iso4217CurrenciesFundsCodes;
+import it.infocert.eigor.model.core.model.*;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.mockito.Mockito.mock;
+
+public class DumpTest {
+
+    @Test
+    public void dumpAnInvoice() {
+
+        // given
+        CoreInvoice invoice = new CoreInvoice();
+
+        // nr. 1
+        BT0001InvoiceNumber invoiceNumber = new BT0001InvoiceNumber("2017/01");
+        invoice.getBT0001InvoiceNumbers().add(invoiceNumber);
+
+        // nr. 5
+        BT0006VatAccountingCurrencyCode currencyCode = new BT0006VatAccountingCurrencyCode(Iso4217CurrenciesFundsCodes.EUR);
+        invoice.getBt0006VatAccountingCurrencyCodes().add(currencyCode);
+
+        // nr. 20
+        BG001InvoiceNote bgInvoiceNote = new BG001InvoiceNote();
+
+        // nr. 21
+        BT0021InvoiceNoteSubjectCode invoiceNoteSubjectCode = new BT0021InvoiceNoteSubjectCode("ABE");
+        bgInvoiceNote.getBt0021InvoiceNoteSubjectCodes().add(invoiceNoteSubjectCode);
+
+        // nr. 22
+        BT0022InvoiceNote btInvoiceNote = new BT0022InvoiceNote("This is a test invoice.");
+        bgInvoiceNote.getBt0022InvoiceNotes().add(btInvoiceNote);
+        invoice.getBg0001InvoiceNotes().add(bgInvoiceNote);
+
+
+        // when
+        Visitor v = mock(Visitor.class);
+        invoice.accept(v);
+
+
+        // then
+        InOrder inOrder = inOrder(v);
+        inOrder.verify(v).startInvoice(invoice);
+
+        inOrder.verify(v).startBTBG(invoiceNumber);
+        inOrder.verify(v).endBTBG(invoiceNumber);
+
+        inOrder.verify(v).startBTBG(currencyCode);
+        inOrder.verify(v).endBTBG(currencyCode);
+
+        inOrder.verify(v).startBTBG(bgInvoiceNote);
+
+        inOrder.verify(v).startBTBG(invoiceNoteSubjectCode);
+        inOrder.verify(v).endBTBG(invoiceNoteSubjectCode);
+
+        inOrder.verify(v).startBTBG(btInvoiceNote);
+        inOrder.verify(v).endBTBG(btInvoiceNote);
+
+        inOrder.verify(v).endBTBG(bgInvoiceNote);
+
+        inOrder.verify(v).endInvoice(invoice);
+
+    }
+
+
+
+}
