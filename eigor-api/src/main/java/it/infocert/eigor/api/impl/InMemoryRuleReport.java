1266434d557cca6520d221cb7f2727fb7b2f2800
Daniele Demichelis <d.demichelis@techgap.it>
2017-03-31 19:20:07 +0200

    Fixed entry problem

--- eigor-api/src/main/java/it/infocert/eigor/api/impl/InMemoryRuleReport.java
+++ eigor-api/src/main/java/it/infocert/eigor/api/impl/InMemoryRuleReport.java
@@ -3,21 +3,23 @@ package it.infocert.eigor.api.impl;
 import it.infocert.eigor.api.RuleReport;
 import it.infocert.eigor.model.core.rules.Rule;
 import it.infocert.eigor.model.core.rules.RuleOutcome;
-import javafx.util.Pair;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class InMemoryRuleReport implements RuleReport {
 
-    private List<Pair<RuleOutcome, Rule>> items = new ArrayList<>();
+    private List<Map.Entry<RuleOutcome, Rule>> items = new ArrayList<>();
 
     @Override public void store(RuleOutcome ruleOutcome, Rule rule) {
-        items.add( new Pair<>(ruleOutcome, rule) );
+        items.add(new AbstractMap.SimpleEntry<>(ruleOutcome, rule) );
     }
 
     public String dump() {
+        Map.Entry<RuleOutcome, Rule> k;
         return "Outcome,Reason\n" +
                 items.stream()
                         .map(x -> x.getKey().outcome() + "," + x.getKey().description())
