e755a0983767c11f211e4886ef1dc1fb8ec45bf8
Matteo Joliveau <m.joliveau@anoki.it>
2017-04-03 12:57:53 +0200

    XML mapping with JAXB is now done automatically by jaxb2-maven-plugin at compile time

--- converter-fattpa-cen/src/main/java/it/infocert/eigor/converter/fattpa2cen/models/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.11 
-// Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2017.03.30 alle 11:53:02 AM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2")
-package it.infocert.eigor.converter.fattpa2cen.models;
