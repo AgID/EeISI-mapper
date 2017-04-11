0574f558cd3f8baf02d12e0d4c5057f520509c33
Matteo Joliveau <m.joliveau@anoki.it>
2017-04-11 09:23:57 +0200

    Finished ShallContainBusinessRule

--- eigor-rules/src/test/java/it/infocert/eigor/rules/business/ShallContainBusinessRuleTest.java
+++ eigor-rules/src/test/java/it/infocert/eigor/rules/business/ShallContainBusinessRuleTest.java
@@ -1,9 +1,6 @@
 package it.infocert.eigor.rules.business;
 
-import it.infocert.eigor.model.core.model.BG0000Invoice;
-import it.infocert.eigor.model.core.model.BG0004Seller;
-import it.infocert.eigor.model.core.model.BT0001InvoiceNumber;
-import it.infocert.eigor.model.core.model.BT0027SellerName;
+import it.infocert.eigor.model.core.model.*;
 import it.infocert.eigor.model.core.rules.RuleOutcome;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,7 +18,7 @@ public class ShallContainBusinessRuleTest {
 
     @Test
     public void failingTest() throws Exception {
-        ShallContainBusinessRule rule = new ShallContainBusinessRule("BT0001", invoice);
+        ShallContainBusinessRule rule = new ShallContainBusinessRule("/BT0001", invoice);
         RuleOutcome compliant = rule.isCompliant(invoice);
         RuleOutcome.Outcome outcome = compliant.outcome();
 
@@ -31,7 +28,7 @@ public class ShallContainBusinessRuleTest {
     @Test
     public void successfulTest() throws Exception {
         invoice.getBT0001InvoiceNumber().add(new BT0001InvoiceNumber("1"));
-        ShallContainBusinessRule rule = new ShallContainBusinessRule("BT0001", invoice);
+        ShallContainBusinessRule rule = new ShallContainBusinessRule("/BT0001", invoice);
         RuleOutcome compliant = rule.isCompliant(invoice);
         RuleOutcome.Outcome outcome = compliant.outcome();
 
@@ -39,15 +36,24 @@ public class ShallContainBusinessRuleTest {
     }
 
     @Test
-    public void complexChainSuccesfulTest() throws Exception {
+    public void complexChainSuccessfulTest() throws Exception {
         BG0004Seller seller = new BG0004Seller();
         seller.getBT0027SellerName().add(new BT0027SellerName("Name"));
         invoice.getBG0004Seller().add(seller);
-        ShallContainBusinessRule rule = new ShallContainBusinessRule("BT0027", invoice);
+        ShallContainBusinessRule rule = new ShallContainBusinessRule("/BG0004/BT0027", invoice);
         RuleOutcome compliant = rule.isCompliant(invoice);
         RuleOutcome.Outcome outcome = compliant.outcome();
 
         assertEquals(RuleOutcome.Outcome.SUCCESS, outcome);
     }
 
+    @Test
+    public void complexChainFailingTest() throws Exception {
+        invoice.getBG0004Seller().add(new BG0004Seller());
+        ShallContainBusinessRule rule = new ShallContainBusinessRule("/BG0004/BT0027", invoice);
+        RuleOutcome compliant = rule.isCompliant(invoice);
+        RuleOutcome.Outcome outcome = compliant.outcome();
+
+        assertEquals(RuleOutcome.Outcome.FAILED, outcome);
+    }
 }
