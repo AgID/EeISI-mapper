5ebe650c812f76588a50d20fde4427d926c6bb75
Matteo Joliveau <m.joliveau@anoki.it>
2017-04-26 16:16:16 +0200

    ConditionalShallContainRule 75% implemented

--- eigor-rules/src/main/java/it/infocert/eigor/rules/constraints/ConditionalShallContainRule.java
+++ eigor-rules/src/main/java/it/infocert/eigor/rules/constraints/ConditionalShallContainRule.java
@@ -9,17 +9,35 @@ public class ConditionalShallContainRule extends ShallContainRule {
 
     private final Reflections reflections;
     private final String invoicePath;
+    private final String conditionPath;
 
     public ConditionalShallContainRule(String invoicePath, String conditionPath, Reflections reflections) {
         super(invoicePath, reflections);
         this.invoicePath = invoicePath;
+        this.conditionPath = conditionPath;
         this.reflections = reflections;
     }
 
     @Override
     public RuleOutcome isCompliant(BG0000Invoice invoice) {
         InvoiceUtils invoiceUtils = new InvoiceUtils(reflections);
+        if (invoiceUtils.hasChild(conditionPath, invoice)) {
+            RuleOutcome ruleOutcome = super.isCompliant(invoice);
+            if (RuleOutcome.Outcome.SUCCESS.equals(ruleOutcome.outcome())) {
+                RuleOutcome.newSuccessOutcome("Since invoice contains %s, it should also contains %s. It does indeed.",
+                        conditionPath.substring(conditionPath.lastIndexOf("/") + 1),
+                        invoicePath.substring(invoicePath.lastIndexOf("/") + 1));
+            } else {
+                RuleOutcome.newFailedOutcome("Since invoice contains %s, it should also contains %s. It does not.",
+                        conditionPath.substring(conditionPath.lastIndexOf("/") + 1),
+                        invoicePath.substring(invoicePath.lastIndexOf("/") + 1));
+            }
+            return ruleOutcome;
+        } else {
+            return RuleOutcome.newFailedOutcome("Invoice doesn't contain %s",
+                    invoicePath.substring(invoicePath.lastIndexOf("/") + 1)); //FIXME How should we treat cases where
+                                                                                  //FIXME the condition isn't required (aka, "if this is present do this, otherwise... (?)
 
-        return super.isCompliant(invoice);
+        }
     }
 }
