09fb5ad2419c6c3a2cd84780999e1d0dae9a9510
Matteo Joliveau <m.joliveau@koor.it>
2017-05-18 15:54:29 +0000

    Better rule outcome and error messages

--- eigor-rules/src/main/java/it/infocert/eigor/rules/integrity/IteratingIntegrityRule.java
+++ eigor-rules/src/main/java/it/infocert/eigor/rules/integrity/IteratingIntegrityRule.java
@@ -6,6 +6,7 @@ import it.infocert.eigor.model.core.model.BG0000Invoice;
 import it.infocert.eigor.model.core.model.BTBG;
 import it.infocert.eigor.model.core.rules.Rule;
 import it.infocert.eigor.model.core.rules.RuleOutcome;
+import it.infocert.eigor.rules.MalformedRuleException;
 
 import javax.el.ELException;
 import javax.el.ExpressionFactory;
@@ -20,6 +21,7 @@ public class IteratingIntegrityRule implements Rule{
     private final ExpressionFactory expressionFactory;
     private final SimpleContext juelContext;
     private final String expression;
+    private final String ruleName;
 
     private static Properties initProps() {
         Properties properties = new Properties();
@@ -27,11 +29,12 @@ public class IteratingIntegrityRule implements Rule{
         return properties;
     }
 
-    public IteratingIntegrityRule(String iterating, String expression) {
+    public IteratingIntegrityRule(String iterating, String expression, String ruleName) throws MalformedRuleException{
         this.expression = expression;
         this.iterating = iterating;
         this.expressionFactory = new ExpressionFactoryImpl(initProps());
         this.juelContext = new SimpleContext();
+        this.ruleName = ruleName;
     }
 
     @Override
@@ -48,29 +51,35 @@ public class IteratingIntegrityRule implements Rule{
                 try {
                     Object tmp = valueExpression.getValue(juelContext);
                     if (Objects.isNull(tmp)) {
-                        outcome = RuleOutcome.newUnapplicableOutcome("Rule is unapplicable");
+                        outcome = RuleOutcome.newUnapplicableOutcome("Rule %s is unapplicable", ruleName);
                         continue;
                     } else {
                         condition = (Boolean) tmp;
                     }
                 } catch (IllegalArgumentException | ClassCastException | ELException e) {
-                    outcome = RuleOutcome.newErrorOutcome("Error in the rule: %s", e.getMessage());
+                    outcome = RuleOutcome.newErrorOutcome("Error in the rule %s: %s", ruleName, e.getMessage());
                     continue;
                 }
 
                 if (condition) {
-                    outcome = RuleOutcome.newSuccessOutcome("Rule successfully validated");
+                    outcome = RuleOutcome.newSuccessOutcome("Rule %s successfully validated", ruleName);
                 } else {
-                    return RuleOutcome.newFailedOutcome("Rule has failed");
+                    return RuleOutcome.newFailedOutcome("Rule %s has failed", ruleName);
                 }
             }
             if (outcome != null) {
                 return outcome;
             } else {
-                return RuleOutcome.newUnapplicableOutcome("Rule is unapplicable, the Iterator is empty");
+                return RuleOutcome.newUnapplicableOutcome("Rule %s is unapplicable, the Iterator is empty", ruleName);
             }
         } else {
-            return RuleOutcome.newErrorOutcome("Error in the items expression. Result is not a valid Iterator");
+            return RuleOutcome.newErrorOutcome("Error in the items expression for rule %s. The expression %s does not return a valid Iterator", ruleName, iterating);
+        }
+    }
+
+    private void validateExpression(String expr) {
+        if (!expr.matches("^(\\$)\\{((?!\\{|}).)*\\}$")) {
+            throw new MalformedRuleException(String.format("Rule %s is malformed: %s. Rule expression should follow the pattern ${ expression } without any surrounding quotes,", ruleName, expr));
         }
     }
 }
