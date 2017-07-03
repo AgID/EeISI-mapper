bb1546957bd3b1fe5cda9fc6f56b4a705a6f9d66
amo <a.mocioi@techgapsolutions.com>
2017-07-04 02:32:59 +0300

    refs !79 fixes #85
    ManyToOne mapping for fromCen transformation

--- eigor-api/src/main/java/it/infocert/eigor/api/mapping/GenericManyToOneTransformer.java
+++ eigor-api/src/main/java/it/infocert/eigor/api/mapping/GenericManyToOneTransformer.java
@@ -1,60 +1,97 @@
 package it.infocert.eigor.api.mapping;
 
 
+import com.google.common.collect.Lists;
 import it.infocert.eigor.api.ConversionIssue;
 import it.infocert.eigor.api.SyntaxErrorInInvoiceFormatException;
 import it.infocert.eigor.api.conversion.ConversionRegistry;
 import it.infocert.eigor.model.core.model.BG0000Invoice;
+import it.infocert.eigor.model.core.model.BTBG;
+import org.jdom2.Element;
 import org.reflections.Reflections;
 import org.slf4j.LoggerFactory;
 import org.jdom2.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenericManyToOneTransformer extends GenericTransformer {
     private final String combinationExpression;
-    private final String cenPath;
-    private final List<String> xPaths;
+    private final String targetPath;
+    private final List<String> sourcePaths;
 
 
     /**
      * Instantiates a new Generic many to one transformation.
      *
-     * @param cenPath the CEN invoice path
+     * @param targetPath the CEN invoice path
      */
-    public GenericManyToOneTransformer(String cenPath, String combinationExpression, List<String> xPaths, Reflections reflections, ConversionRegistry conversionRegistry) {
+    public GenericManyToOneTransformer(String targetPath, String combinationExpression, List<String> sourcePaths, Reflections reflections, ConversionRegistry conversionRegistry) {
         super(reflections, conversionRegistry);
-        this.cenPath = cenPath;
+        this.targetPath = targetPath;
         this.combinationExpression = combinationExpression;
-        this.xPaths = xPaths;
+        this.sourcePaths = sourcePaths;
         log = LoggerFactory.getLogger(GenericManyToOneTransformer.class);
     }
 
 
 
     @Override
-    public void transformXmlToCen(Document document, BG0000Invoice invoice, final List<ConversionIssue> errors) throws SyntaxErrorInInvoiceFormatException {
-        final String logPrefix = "(" + xPaths + " - " + cenPath + ") ";
+    public void transformXmlToCen(Document document, BG0000Invoice invoice, List<ConversionIssue> errors) throws SyntaxErrorInInvoiceFormatException {
+        final String logPrefix = "(" + sourcePaths + " - " + targetPath + ") ";
         log.info(logPrefix + "resolving");
 
         String finalValue = combinationExpression;
-        for (int idx=0; idx<xPaths.size(); idx++){
+        for (int idx = 0; idx< sourcePaths.size(); idx++){
 
-            String xPathText = getNodeTextFromXPath(document, xPaths.get(idx));
+            String xPathText = getNodeTextFromXPath(document, sourcePaths.get(idx));
             if (xPathText != null) {
                 finalValue = finalValue.replace("%"+(idx+1), xPathText);
             }
         }
 
-        if (finalValue.indexOf("%") != -1){
+        if (finalValue.contains("%")){
             errors.add(ConversionIssue.newWarning(new RuntimeException("Source element missing to complete expression: " + combinationExpression + "; Result: " + finalValue)));
         } else {
-            addNewCenObjectFromStringValueToInvoice(cenPath, invoice, finalValue, errors);
+            addNewCenObjectFromStringValueToInvoice(targetPath, invoice, finalValue, errors);
         }
     }
 
     @Override
     public void transformCenToXml(BG0000Invoice invoice, Document document, List<ConversionIssue> errors) throws SyntaxErrorInInvoiceFormatException {
-        throw new RuntimeException("Not yet implemented!");
+        final String logPrefix = "(" + sourcePaths + " - " + targetPath + ") ";
+        log.trace(logPrefix + "resolving");
+
+        String finalValue = combinationExpression;
+        for (int idx = 0; idx< sourcePaths.size(); idx++){
+
+            List<BTBG> bts = getAllBTs(sourcePaths.get(idx), invoice, errors);
+            if (bts == null || bts.size() == 0) return;
+            if (bts.size() > 1) {
+                errors.add(ConversionIssue.newError(new RuntimeException("More than one BT for " + sourcePaths.get(idx) + ": " + bts)));
+                return;
+            }
+            BTBG btbg = bts.get(0);
+            Object value = getBtValue(btbg, errors);
+            if (value != null) {
+                Class<?> aClass = value.getClass();
+                String converted = conversionRegistry.convert(aClass, String.class, value);
+                log.info("CEN '{}' with value '{}' mapped to XML element '{}' with value '{}'.",
+                        btbg.denomination(), String.valueOf(value), targetPath, converted);
+                finalValue = finalValue.replace("%"+(idx+1), converted);
+            }
+        }
+
+        if (finalValue.contains("%")){
+            errors.add(ConversionIssue.newWarning(new RuntimeException("Source element missing to complete expression: " + combinationExpression + "; Result: " + finalValue)));
+        } else {
+            List<Element> elements = getAllXmlElements(targetPath, document, 1, sourcePaths.toString(), errors);
+            if (elements == null) return;
+            if (elements.size() > 1) {
+                errors.add(ConversionIssue.newError(new RuntimeException("More than one element for " + targetPath + ": " + elements)));
+                return;
+            }
+            elements.get(0).setText(finalValue);
+        }
     }
 }
--- eigor-api/src/main/java/it/infocert/eigor/api/mapping/GenericTransformer.java
+++ eigor-api/src/main/java/it/infocert/eigor/api/mapping/GenericTransformer.java
@@ -2,6 +2,7 @@ package it.infocert.eigor.api.mapping;
 
 import com.amoerie.jstreams.Stream;
 import com.amoerie.jstreams.functions.Consumer;
+import com.google.common.collect.Lists;
 import it.infocert.eigor.api.ConversionIssue;
 import it.infocert.eigor.api.SyntaxErrorInInvoiceFormatException;
 import it.infocert.eigor.api.conversion.ConversionRegistry;
@@ -15,6 +16,7 @@ import org.slf4j.Logger;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,6 +40,16 @@ public abstract class GenericTransformer {
             log.trace(xPath + "item found: " + item);
         }
         return item;
+    }
+
+    protected Object getBtValue(BTBG btbg, List<ConversionIssue> errors) {
+        try {
+            Method getValue = btbg.getClass().getDeclaredMethod("getValue");
+            return getValue.invoke(btbg);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            errors.add(ConversionIssue.newError(e));
+            return null;
+        }
     }
 
     protected Object addNewCenObjectFromStringValueToInvoice(String cenPath, BG0000Invoice invoice, final String xPathText, final List<ConversionIssue> errors) {
@@ -114,6 +126,108 @@ public abstract class GenericTransformer {
             }
         }
         return constructorParam[0];
+    }
+
+    private List<BTBG> getBtRecursively(BTBG parent, final ArrayList<String> steps, final List<BTBG> bts) {
+        List<BTBG> childrenAsList = invoiceUtils.getChildrenAsList(parent, steps.remove(0));
+        for (BTBG btbg : childrenAsList) {
+            if (btbg.getClass().getSimpleName().startsWith("BG")) {
+                getBtRecursively(btbg, (ArrayList<String>) steps.clone(), bts);
+            } else {
+                bts.add(btbg);
+            }
+        }
+
+        return bts;
+    }
+
+    protected List<Element> getAllXmlElements (String xPath, Document document, int btsSize, String cenPath, List<ConversionIssue> errors) {
+        ArrayList<String> xmlSteps = Lists.newArrayList(xPath.substring(1).split("/"));
+        String remove //keep it that way, there were some access races without it that still need to be investigated
+                = xmlSteps.remove(0);
+        List<Element> elements = createXmlPathRecursively(document.getRootElement(), xmlSteps, btsSize, new ArrayList<Element>(0));
+        if (btsSize > elements.size()) {
+            ConversionIssue e = ConversionIssue.newError(
+                    new IllegalArgumentException("BTs can not be more than XML elements"),
+                    String.format("Found %d %s but only %d %s XML elements were created. " +
+                                    "Maybe there is an error in the configuration file or the converted CEN object is not well formed. " +
+                                    "Check rule-report.csv for more informations about BT/BGs validation",
+                            btsSize,
+                            cenPath,
+                            elements.size(),
+                            xPath
+                    )
+            );
+            errors.add(e);
+            log.error(e.getMessage());
+            return null;
+        }
+        return elements;
+    }
+
+    protected List<BTBG> getAllBTs(String cenPath, BG0000Invoice invoice, final List<ConversionIssue> errors) {
+        String[] cenSteps = cenPath.substring(1).split("/");
+        List<BTBG> bts;
+        try {
+            bts = getBtRecursively(invoice, Lists.newArrayList(cenSteps), new ArrayList<BTBG>(0));
+        } catch (Exception e) {
+            errors.add(ConversionIssue.newError(e, e.getMessage()));
+            return null;
+        }
+        return bts;
+    }
+
+    protected synchronized List<Element> createXmlPathRecursively(Element parent, final ArrayList<String> steps, final int times, final List<Element> leafs) {
+        if (times == 0) {
+            return leafs;
+        }
+
+        boolean last = false;
+        if (steps.size() == 1) {
+            last = true;
+        }
+        if (!steps.isEmpty()) {
+            String tagname = steps.remove(0);
+            if (tagname.contains("[") && tagname.contains("]")) {
+                tagname = tagname.substring(0, tagname.indexOf("["));
+                List<Element> children = parent.getChildren(tagname);
+                ArrayList<Element> elements = new ArrayList<>(times);
+                int loop = 0;
+                if (children.size() < times) {
+                    loop = times - children.size();
+                } else if (children.isEmpty()) {
+                    loop = times;
+                }
+                elements.addAll(children);
+                for (int i = 0; i < loop; i++) {
+                    Element el = new Element(tagname);
+                    parent.addContent(el);
+                    elements.add(el);
+                }
+
+                for (Element element : elements) {
+                    if (last) {
+                        leafs.add(element);
+                    } else {
+                        createXmlPathRecursively(element, (ArrayList<String>) steps.clone(), times, leafs);
+                    }
+                }
+            } else {
+                Element child = parent.getChild(tagname);
+                if (child == null) {
+                    child = new Element(tagname);
+                    parent.addContent(child);
+                }
+                if (last) {
+                    leafs.add(child);
+                } else {
+                    createXmlPathRecursively(child, (ArrayList<String>) steps.clone(), times, leafs);
+                }
+            }
+
+        }
+
+        return leafs;
     }
 
     public abstract void transformXmlToCen(Document document, BG0000Invoice invoice, final List<ConversionIssue> errors) throws SyntaxErrorInInvoiceFormatException;
