92bde16bb17368cf665ed98d0fe91e0579f8cc51
Daniele Demichelis <d.demichelis@techgap.it>
2017-08-07 12:17:34 +0200

    Fixing pages build borken.

---
+++ eigor-spring-core/src/test/java/it/infocert/eigor/org/springframework/EmptyTest.java
@@ -0,0 +1,16 @@
+package it.infocert.eigor.org.springframework;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmptyTest {
+
+    @Test
+    public void emptyTest() {
+
+        // This test is here just to generate the pit reports.
+
+        Assert.assertTrue(true);
+    }
+
+}
