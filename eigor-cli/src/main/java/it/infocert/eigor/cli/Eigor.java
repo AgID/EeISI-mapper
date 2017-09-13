16e3678ec2097187a392754de903a4cb0459ff1b
Matteo Joliveau <m.joliveau@koor.it>
2017-09-13 18:10:03 +0200

    Nuked old FattPa2Cen

--- eigor-cli/src/main/java/it/infocert/eigor/cli/Eigor.java
+++ eigor-cli/src/main/java/it/infocert/eigor/cli/Eigor.java
@@ -12,7 +12,6 @@ import it.infocert.eigor.api.impl.ToCenListBakedRepository;
 import it.infocert.eigor.converter.cen2fattpa.Cen2FattPA;
 import it.infocert.eigor.converter.cii2cen.Cii2Cen;
 import it.infocert.eigor.converter.csvcen2cen.CsvCen2Cen;
-import it.infocert.eigor.converter.fattpa2cen.FattPA2CenConverter;
 import it.infocert.eigor.converter.ubl2cen.Ubl2Cen;
 import it.infocert.eigor.rules.repositories.CardinalityRulesRepository;
 import it.infocert.eigor.rules.repositories.CompositeRuleRepository;
@@ -88,7 +87,6 @@ public class Eigor {
     ToCenConversionRepository toCenConversionRepository(Reflections reflections, EigorConfiguration configuration) {
         return new ToCenListBakedRepository(
                 new Ubl2Cen(reflections, configuration),
-                new FattPA2CenConverter(reflections, configuration),
                 new CsvCen2Cen(reflections),
                 new Cii2Cen(reflections, configuration)
         );
