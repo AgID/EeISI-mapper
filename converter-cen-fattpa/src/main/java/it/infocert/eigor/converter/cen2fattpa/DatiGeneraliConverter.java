bacc4cf4bdf9a444cb2ac1154bf689340c2da365
Matteo Joliveau <m.joliveau@koor.it>
2017-10-26 12:30:55 +0200

    Added DatiFattureCollegate (BT-25 and 26), closes #143

--- converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/DatiGeneraliConverter.java
+++ converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/DatiGeneraliConverter.java
@@ -2,16 +2,22 @@ package it.infocert.eigor.converter.cen2fattpa;
 
 import it.infocert.eigor.api.ConversionIssue;
 import it.infocert.eigor.api.CustomMapping;
+import it.infocert.eigor.api.EigorRuntimeException;
 import it.infocert.eigor.api.IConversionIssue;
+import it.infocert.eigor.api.conversion.LocalDateToXMLGregorianCalendarConverter;
 import it.infocert.eigor.converter.cen2fattpa.models.*;
 import it.infocert.eigor.model.core.model.*;
+import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.List;
 
 public class DatiGeneraliConverter implements CustomMapping<FatturaElettronicaType> {
     private static final Logger log = LoggerFactory.getLogger(DatiGeneraliConverter.class);
+
+    private final LocalDateToXMLGregorianCalendarConverter dateConverter = new LocalDateToXMLGregorianCalendarConverter();
 
     @Override
     public void map(BG0000Invoice invoice, FatturaElettronicaType fatturaElettronica, List<IConversionIssue> errors) {
@@ -23,13 +29,14 @@ public class DatiGeneraliConverter implements CustomMapping<FatturaElettronicaTy
             errors.add(ConversionIssue.newError(new IllegalArgumentException("No FatturaElettronicaBody found in current FatturaElettronica")));
         } else {
             FatturaElettronicaBodyType fatturaElettronicaBody = bodies.get(0);
-            addCausale(invoice, fatturaElettronicaBody, errors);
+            DatiGeneraliType datiGenerali = fatturaElettronicaBody.getDatiGenerali();
+            addCausale(invoice, datiGenerali, errors);
+            addFattureCollegate(invoice, datiGenerali, errors);
         }
     }
 
 
-    private void addCausale(BG0000Invoice invoice, FatturaElettronicaBodyType body, List<IConversionIssue> errors) {
-        DatiGeneraliType datiGenerali = body.getDatiGenerali();
+    private void addCausale(BG0000Invoice invoice, DatiGeneraliType datiGenerali, List<IConversionIssue> errors) {
         if (datiGenerali != null) {
             DatiGeneraliDocumentoType datiGeneraliDocumento = datiGenerali.getDatiGeneraliDocumento();
             if (datiGeneraliDocumento != null) {
@@ -85,9 +92,34 @@ public class DatiGeneraliConverter implements CustomMapping<FatturaElettronicaTy
 
     }
 
+    private void addFattureCollegate(BG0000Invoice invoice, DatiGeneraliType datiGenerali, List<IConversionIssue> errors) {
+        if (!invoice.getBG0003PrecedingInvoiceReference().isEmpty()) {
+            List<DatiDocumentiCorrelatiType> datiFattureCollegate = datiGenerali.getDatiFattureCollegate();
+            for (BG0003PrecedingInvoiceReference precedingInvoiceReference : invoice.getBG0003PrecedingInvoiceReference()) {
+                DatiDocumentiCorrelatiType fatturaCollegata = new DatiDocumentiCorrelatiType();
+                datiFattureCollegate.add(fatturaCollegata);
+
+                if (!precedingInvoiceReference.getBT0025PrecedingInvoiceReference().isEmpty()) {
+                    BT0025PrecedingInvoiceReference invoiceReference = precedingInvoiceReference.getBT0025PrecedingInvoiceReference(0);
+                    fatturaCollegata.setIdDocumento(invoiceReference.getValue());
+                }
+
+                if (!precedingInvoiceReference.getBT0026PrecedingInvoiceIssueDate().isEmpty()) {
+                    try {
+                        XMLGregorianCalendar precedingInvoiceIssueDate = dateConverter.convert(precedingInvoiceReference.getBT0026PrecedingInvoiceIssueDate(0).getValue());
+                        fatturaCollegata.setData(precedingInvoiceIssueDate);
+                    } catch (EigorRuntimeException e) {
+                        errors.add(ConversionIssue.newError(e));
+                    }
+                }
+            }
+        }
+
+    }
+
     private void manageNoteText(DatiGeneraliDocumentoType datiGeneraliDocumento, String noteText) {
         if (noteText.length() > 200) {
-            datiGeneraliDocumento.getCausale().add(noteText.substring(0,200));
+            datiGeneraliDocumento.getCausale().add(noteText.substring(0, 200));
             manageNoteText(datiGeneraliDocumento, noteText.substring(200));
         } else {
             datiGeneraliDocumento.getCausale().add(noteText);
