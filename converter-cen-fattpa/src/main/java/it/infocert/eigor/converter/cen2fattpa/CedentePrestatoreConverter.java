eebe60495ec0a082c58ab36ce10897df91dfd77e
Matteo Joliveau <m.joliveau@koor.it>
2017-12-18 15:53:35 +0000

    Finishing custom mapping for non-italian addresses

--- converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/CedentePrestatoreConverter.java
+++ converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/CedentePrestatoreConverter.java
@@ -2,9 +2,13 @@ package it.infocert.eigor.converter.cen2fattpa;
 
 import com.amoerie.jstreams.Stream;
 import com.amoerie.jstreams.functions.Filter;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import it.infocert.eigor.api.ConversionIssue;
 import it.infocert.eigor.api.CustomMapping;
+import it.infocert.eigor.api.EigorException;
 import it.infocert.eigor.api.IConversionIssue;
+import it.infocert.eigor.api.errors.ErrorMessage;
 import it.infocert.eigor.converter.cen2fattpa.models.*;
 import it.infocert.eigor.model.core.datatypes.Identifier;
 import it.infocert.eigor.model.core.enums.Iso31661CountryCodes;
@@ -16,6 +20,11 @@ import java.util.List;
 
 public class CedentePrestatoreConverter implements CustomMapping<FatturaElettronicaType> {
     private static final Logger log = LoggerFactory.getLogger(CedentePrestatoreConverter.class);
+    private final AttachmentUtil attachmentUtil;
+
+    public CedentePrestatoreConverter() {
+        attachmentUtil = new AttachmentUtil();
+    }
 
     @Override
     public void map(BG0000Invoice invoice, FatturaElettronicaType fatturaElettronica, List<IConversionIssue> errors) {
@@ -27,6 +36,7 @@ public class CedentePrestatoreConverter implements CustomMapping<FatturaElettron
                 FatturaElettronicaBodyType body = bodies.get(0);
                 mapBt29(invoice, body, cedentePrestatore, errors);
                 mapBt30(invoice, body, cedentePrestatore, errors);
+                addIndirizzo(invoice, fatturaElettronica, errors);
             }
         } else {
             errors.add(ConversionIssue.newError(new IllegalArgumentException("No CedentePrestatore was found in current FatturaElettronicaHeader")));
@@ -202,6 +212,90 @@ public class CedentePrestatoreConverter implements CustomMapping<FatturaElettron
 
     }
 
+    private void addIndirizzo(BG0000Invoice invoice, FatturaElettronicaType fatturaElettronica, List<IConversionIssue> errors) {
+        final CedentePrestatoreType cedentePrestatore = fatturaElettronica.getFatturaElettronicaHeader().getCedentePrestatore();
+        final List<BG0004Seller> sellers = invoice.getBG0004Seller();
+        if (!sellers.isEmpty()) {
+            final List<BG0005SellerPostalAddress> addresses = sellers.get(0).getBG0005SellerPostalAddress();
+            if (!addresses.isEmpty()) {
+                final BG0005SellerPostalAddress address = addresses.get(0);
+
+                final List<BT0040SellerCountryCode> countryCodes = address.getBT0040SellerCountryCode();
+                if (!countryCodes.isEmpty() && !Iso31661CountryCodes.IT.equals(countryCodes.get(0).getValue())) {
+                    log.debug("Non-italian address, applying custom mapping");
+                    final List<BT0038SellerPostCode> postCodes = address.getBT0038SellerPostCode();
+
+                    final List<BT0035SellerAddressLine1> addressLines1 = address.getBT0035SellerAddressLine1();
+                    final List<BT0036SellerAddressLine2> addressLines2 = address.getBT0036SellerAddressLine2();
+                    final List<BT0162SellerAddressLine3> addressLines3 = address.getBT0162SellerAddressLine3();
+                    final StringBuilder sb = new StringBuilder();
+                    String addressLine1Value = "";
+                    String addressLine2Value = "";
+                    String addressLine3Value = "";
+                    if (!addressLines1.isEmpty()) {
+                        final BT0035SellerAddressLine1 addressLine1 = addressLines1.get(0);
+                        addressLine1Value = addressLine1.getValue();
+                    } else {
+                        log.warn("No [BT-35] SellerAddressLine1 was found in current [BG-5] SellerPostalAddress");
+                    }
+
+                    if (!addressLines2.isEmpty()) {
+                        final BT0036SellerAddressLine2 addressLine2 = addressLines2.get(0);
+                        addressLine2Value = addressLine2.getValue();
+                    } else {
+                        log.warn("No [BT-36] SellerAddressLine2 was found in current [BG-5] SellerPostalAddress");
+                    }
+
+                    if (!addressLines3.isEmpty()) {
+                        final BT0036SellerAddressLine2 addressLine3 = addressLines2.get(0);
+                        addressLine3Value = addressLine3.getValue();
+                    } else {
+                        log.warn("No [BT-162] SellerAddressLine3 was found in current [BG-5] SellerPostalAddress");
+                    }
+
+                    final IndirizzoType sede = Optional.fromNullable(cedentePrestatore.getSede()).or(new IndirizzoType());
+                    for (String s : Lists.newArrayList(addressLine1Value, addressLine2Value, addressLine3Value)) {
+                        sb.append(s).append(IConstants.WHITESPACE);
+                    }
+                    final String addressIt = sb.toString().trim();
+                    final FatturaElettronicaBodyType body = fatturaElettronica.getFatturaElettronicaBody().get(0);
+                    if (addressIt.length() > 60) {
+                        final String first = addressIt.substring(0, 59);
+                        sede.setIndirizzo(first);
+                        attachmentUtil.addToUnmappedValuesAttachment(fatturaElettronica.getFatturaElettronicaBody().get(0), "BT0035: " + addressLine1Value);
+                        attachmentUtil.addToUnmappedValuesAttachment(fatturaElettronica.getFatturaElettronicaBody().get(0), "BT0036: " + addressLine2Value);
+                        attachmentUtil.addToUnmappedValuesAttachment(fatturaElettronica.getFatturaElettronicaBody().get(0), "BT0162: " + addressLine3Value);
+//                        errors.add(ConversionIssue.newWarning(new EigorException(new ErrorMessage("SellerAddress was not compliant with FatturaPA specification. " +
+//                                "Address has been truncated to the first 60 characters. See not-mapped-values.txt in attachment for the original values"))));
+                        log.warn("SellerAddress was not compliant with FatturaPA specification. " +
+                                "Address has been truncated to the first 60 characters. See not-mapped-values.txt in attachment for the original values");
+                    } else {
+                        sede.setIndirizzo(addressIt);
+                    }
+
+                    if (!postCodes.isEmpty()) {
+                        final String postCode = postCodes.get(0).getValue();
+                        attachmentUtil.addToUnmappedValuesAttachment(body, "BT0038: " + postCode);
+                        sede.setCAP("99999");
+//                        errors.add(ConversionIssue.newWarning(new EigorException(new ErrorMessage("SellerPostalCode was not compliant with FatturaPA specification. " +
+//                                "PostalCode has been replaced with placeholder. See not-mapped-values.txt in attachment for the original values"))));
+                        log.warn("SellerPostalCode was not compliant with FatturaPA specification. " +
+                                "PostalCode has been replaced with placeholder. See not-mapped-values.txt in attachment for the original values");
+                    } else {
+                        log.warn("No [BT-38] SellerPostCode was found in current [BG-5] SellerPostalAddress");
+                    }
+                } else {
+                    log.debug("Italian address, keeping normal mapping");
+                }
+
+            } else {
+                log.warn("No [BG-5] SellerPostalAddress was found in current [BG-4] Seller");
+            }
+        } else {
+            log.warn("No [BG-4] Seller was found in current Invoice");
+        }
+    }
+
     private void addRegimeFiscale(BG0000Invoice invoice, CedentePrestatoreType cedentePrestatore, List<IConversionIssue> errors) {
         if (!invoice.getBG0004Seller().isEmpty()) {
             BG0004Seller seller = invoice.getBG0004Seller(0);
@@ -226,7 +320,7 @@ public class CedentePrestatoreConverter implements CustomMapping<FatturaElettron
                 errors.add(ConversionIssue.newError(new IllegalArgumentException("No DatiAnagrafici was found in current CedentePrestatore")));
             }
         } else {
-            errors.add(ConversionIssue.newError(new IllegalArgumentException("No CedentePrestatore was found in current FatturaElettronicaHeader")));
+            errors.add(ConversionIssue.newError(new IllegalArgumentException("No [BG-4] Seller was found in current Invoice")));
         }
     }
 
