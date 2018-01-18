0b2bcfdd62abfb9d3ada47ec7473b00042233d8c
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-01-18 18:14:24 +0200

    move TaxPointDate mapping from one2one to custom converter

--- converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/VATBreakdownConverter.java
+++ converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/VATBreakdownConverter.java
@@ -3,12 +3,15 @@ package it.infocert.eigor.converter.cen2cii;
 import it.infocert.eigor.api.*;
 import it.infocert.eigor.api.conversion.ConversionFailedException;
 import it.infocert.eigor.api.conversion.DoubleToStringConverter;
+import it.infocert.eigor.api.conversion.JavaLocalDateToStringConverter;
 import it.infocert.eigor.api.conversion.TypeConverter;
 import it.infocert.eigor.api.errors.ErrorMessage;
+import it.infocert.eigor.model.core.enums.Untdid2005DateTimePeriodQualifiers;
 import it.infocert.eigor.model.core.model.*;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
+import org.joda.time.LocalDate;
 
 import java.util.List;
 
@@ -20,34 +23,67 @@ public class VATBreakdownConverter extends CustomConverterUtils implements Custo
     @Override
     public void map(BG0000Invoice cenInvoice, Document document, List<IConversionIssue> errors) {
         TypeConverter<Double, String> dblStrConverter = DoubleToStringConverter.newConverter("0.00");
+        TypeConverter<LocalDate, String> dateStrConverter = JavaLocalDateToStringConverter.newConverter("yyyyMMdd");
 
         Element rootElement = document.getRootElement();
         List<Namespace> namespacesInScope = rootElement.getNamespacesIntroduced();
+        Namespace ramNs = rootElement.getNamespace("ram");
+        Namespace udtNs = rootElement.getNamespace("udt");
 
         Element supplyChainTradeTransaction = findNamespaceChild(rootElement, namespacesInScope, "SupplyChainTradeTransaction");
         Element applicableHeaderTradeSettlement = null;
 
-        if(supplyChainTradeTransaction == null){
+        if (supplyChainTradeTransaction == null) {
             supplyChainTradeTransaction = new Element("SupplyChainTradeTransaction", rootElement.getNamespace("rsm"));
-            applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", rootElement.getNamespace("ram"));
+            applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", ramNs);
             supplyChainTradeTransaction.addContent(applicableHeaderTradeSettlement);
             rootElement.addContent(supplyChainTradeTransaction);
         } else {
             applicableHeaderTradeSettlement = findNamespaceChild(supplyChainTradeTransaction, namespacesInScope, "ApplicableHeaderTradeSettlement");
-            if(applicableHeaderTradeSettlement == null){
-                applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", rootElement.getNamespace("ram"));
+            if (applicableHeaderTradeSettlement == null) {
+                applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", ramNs);
                 supplyChainTradeTransaction.addContent(applicableHeaderTradeSettlement);
             }
         }
 
+        Element taxPointDate = null;
+        if (!cenInvoice.getBT0007ValueAddedTaxPointDate().isEmpty()) {
+            LocalDate bt0007 = cenInvoice.getBT0007ValueAddedTaxPointDate(0).getValue();
+            Element dateString = new Element("DateString", udtNs);
+            dateString.setAttribute("format", "102");
+            try {
+                dateString.setText(dateStrConverter.convert(bt0007));
+                taxPointDate = new Element("TaxPointDate", ramNs);
+                taxPointDate.addContent(dateString);
+            } catch (ConversionFailedException e) {
+                EigorRuntimeException ere = new EigorRuntimeException(e, ErrorMessage.builder().message("Invalid date format").action("VATBreakdownConverter").build());
+                errors.add(ConversionIssue.newError(ere));
+            }
+        }
+
+        Element dueDateTypeCode = null;
+        if (!cenInvoice.getBT0008ValueAddedTaxPointDateCode().isEmpty()) {
+            Untdid2005DateTimePeriodQualifiers bt0008 = cenInvoice.getBT0008ValueAddedTaxPointDateCode(0).getValue();
+            dueDateTypeCode = new Element("DueDateTypeCode", ramNs);
+            dueDateTypeCode.setText(String.valueOf(bt0008.getCode()));
+        }
+
         for (BG0023VatBreakdown bg0023 : cenInvoice.getBG0023VatBreakdown()) {
 
-            Element applicableTradeTax = new Element("ApplicableTradeTax", rootElement.getNamespace("ram"));
+            Element applicableTradeTax = new Element("ApplicableTradeTax", ramNs);
+
+            if (taxPointDate != null) {
+                applicableTradeTax.addContent(taxPointDate);
+            }
+
+            if (dueDateTypeCode != null) {
+                applicableTradeTax.addContent(dueDateTypeCode);
+            }
 
             if (!bg0023.getBT0116VatCategoryTaxableAmount().isEmpty()) {
                 BT0116VatCategoryTaxableAmount bt0116 = bg0023.getBT0116VatCategoryTaxableAmount(0);
                 try {
-                    Element basisAmount = new Element("BasisAmount", rootElement.getNamespace("ram"));
+                    Element basisAmount = new Element("BasisAmount", ramNs);
                     basisAmount.setText(dblStrConverter.convert(bt0116.getValue()));
                     applicableTradeTax.addContent(basisAmount);
                 } catch (NumberFormatException | ConversionFailedException e) {
@@ -59,7 +95,7 @@ public class VATBreakdownConverter extends CustomConverterUtils implements Custo
             if (!bg0023.getBT0117VatCategoryTaxAmount().isEmpty()) {
                 BT0117VatCategoryTaxAmount bt0117 = bg0023.getBT0117VatCategoryTaxAmount(0);
                 try {
-                    Element calculatedAmount = new Element("CalculatedAmount", rootElement.getNamespace("ram"));
+                    Element calculatedAmount = new Element("CalculatedAmount", ramNs);
                     calculatedAmount.setText(dblStrConverter.convert(bt0117.getValue()));
                     applicableTradeTax.addContent(calculatedAmount);
                 } catch (NumberFormatException | ConversionFailedException e) {
@@ -72,11 +108,11 @@ public class VATBreakdownConverter extends CustomConverterUtils implements Custo
             if (!bg0023.getBT0118VatCategoryCode().isEmpty()) {
                 BT0118VatCategoryCode bt0118 = bg0023.getBT0118VatCategoryCode(0);
 
-                Element typeCode = new Element("TypeCode", rootElement.getNamespace("ram"));
+                Element typeCode = new Element("TypeCode", ramNs);
                 typeCode.setText("VAT");
                 applicableTradeTax.addContent(typeCode);
 
-                Element categoryCode = new Element("CategoryCode", rootElement.getNamespace("ram"));
+                Element categoryCode = new Element("CategoryCode", ramNs);
                 categoryCode.setText(bt0118.getValue().name());
                 applicableTradeTax.addContent(categoryCode);
             }
@@ -84,7 +120,7 @@ public class VATBreakdownConverter extends CustomConverterUtils implements Custo
             if (!bg0023.getBT0119VatCategoryRate().isEmpty()) {
                 BT0119VatCategoryRate bt0119 = bg0023.getBT0119VatCategoryRate(0);
                 try {
-                    Element rateApplicablePercent = new Element("RateApplicablePercent", rootElement.getNamespace("ram"));
+                    Element rateApplicablePercent = new Element("RateApplicablePercent", ramNs);
                     rateApplicablePercent.setText(dblStrConverter.convert(bt0119.getValue()));
                     applicableTradeTax.addContent(rateApplicablePercent);
                 } catch (NumberFormatException | ConversionFailedException e) {
@@ -95,14 +131,14 @@ public class VATBreakdownConverter extends CustomConverterUtils implements Custo
 
             if (!bg0023.getBT0120VatExemptionReasonText().isEmpty()) {
                 BT0120VatExemptionReasonText bt0120 = bg0023.getBT0120VatExemptionReasonText(0);
-                Element exemptionReason = new Element("ExemptionReason", rootElement.getNamespace("ram"));
+                Element exemptionReason = new Element("ExemptionReason", ramNs);
                 exemptionReason.setText(bt0120.getValue());
                 applicableTradeTax.addContent(exemptionReason);
             }
 
             if (!bg0023.getBT0121VatExemptionReasonCode().isEmpty()) {
                 BT0121VatExemptionReasonCode bt0121 = bg0023.getBT0121VatExemptionReasonCode(0);
-                Element exemptionReasonCode = new Element("ExemptionReasonCode", rootElement.getNamespace("ram"));
+                Element exemptionReasonCode = new Element("ExemptionReasonCode", ramNs);
                 exemptionReasonCode.setText(bt0121.getValue().name());
                 applicableTradeTax.addContent(exemptionReasonCode);
             }
