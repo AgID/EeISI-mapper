1156fe1277098fa58332adad1f03f56a68345367
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-01-18 17:16:07 +0200

    move invoice id and issuedate from one2one to custom converter

--- converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/InvoiceNoteConverter.java
+++ converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/InvoiceNoteConverter.java
@@ -1,13 +1,17 @@
 package it.infocert.eigor.converter.cen2cii;
 
-import it.infocert.eigor.api.CustomConverterUtils;
-import it.infocert.eigor.api.CustomMapping;
-import it.infocert.eigor.api.IConversionIssue;
+import it.infocert.eigor.api.*;
+import it.infocert.eigor.api.conversion.ConversionFailedException;
+import it.infocert.eigor.api.conversion.JavaLocalDateToStringConverter;
+import it.infocert.eigor.api.conversion.TypeConverter;
+import it.infocert.eigor.api.errors.ErrorMessage;
 import it.infocert.eigor.model.core.model.BG0000Invoice;
 import it.infocert.eigor.model.core.model.BG0001InvoiceNote;
+import it.infocert.eigor.model.core.model.BT0001InvoiceNumber;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
+import org.joda.time.LocalDate;
 
 import java.util.List;
 
@@ -19,8 +23,12 @@ public class InvoiceNoteConverter extends CustomConverterUtils implements Custom
     @Override
     public void map(BG0000Invoice cenInvoice, Document document, List<IConversionIssue> errors) {
         if (!cenInvoice.getBG0001InvoiceNote().isEmpty()) {
+            TypeConverter<LocalDate, String> dateStrConverter = JavaLocalDateToStringConverter.newConverter("yyyyMMdd");
+
             Element rootElement = document.getRootElement();
             List<Namespace> namespacesInScope = rootElement.getNamespacesIntroduced();
+            Namespace ramNs = rootElement.getNamespace("ram");
+            Namespace udtNs = rootElement.getNamespace("udt");
 
             Element exchangedDocument = findNamespaceChild(rootElement, namespacesInScope, "ExchangedDocument");
 
@@ -29,24 +37,46 @@ public class InvoiceNoteConverter extends CustomConverterUtils implements Custom
                 rootElement.addContent(exchangedDocument);
             }
 
+            if (!cenInvoice.getBT0001InvoiceNumber().isEmpty()) {
+                String bt0001 = cenInvoice.getBT0001InvoiceNumber(0).getValue();
+                Element id = new Element("ID", ramNs);
+                id.setText(bt0001);
+                exchangedDocument.addContent(id);
+            }
+
+            if (!cenInvoice.getBT0002InvoiceIssueDate().isEmpty()) {
+                LocalDate bt0002 = cenInvoice.getBT0002InvoiceIssueDate(0).getValue();
+                Element issueDateTime = new Element("IssueDateTime", ramNs);
+                Element dateTimeString = new Element("DateTimeString", udtNs);
+                dateTimeString.setAttribute("format", "102");
+                try {
+                    dateTimeString.setText(dateStrConverter.convert(bt0002));
+                    issueDateTime.addContent(dateTimeString);
+                    exchangedDocument.addContent(issueDateTime);
+                } catch (IllegalArgumentException | ConversionFailedException e) {
+                    EigorRuntimeException ere = new EigorRuntimeException(e, ErrorMessage.builder().message("Invalid date format").action("InvoiceNoteConverter").build());
+                    errors.add(ConversionIssue.newError(ere));
+                }
+            }
+
             List<BG0001InvoiceNote> bg0001InvoiceNote = cenInvoice.getBG0001InvoiceNote();
             for (int i = 0; i < bg0001InvoiceNote.size(); i++) {
                 BG0001InvoiceNote bg0001 = bg0001InvoiceNote.get(i);
-                Element includedNote = new Element("IncludedNote", rootElement.getNamespace("ram"));
+                Element includedNote = new Element("IncludedNote", ramNs);
 
                 // FIXME according to CII XSD, ID is mandatory, but we have no mapping, thus index is used as mock value
-                Element id = new Element("ID", rootElement.getNamespace("ram"));
+                Element id = new Element("ID", ramNs);
                 id.setText(String.valueOf(i));
                 includedNote.addContent(id);
 
                 if (!bg0001.getBT0021InvoiceNoteSubjectCode().isEmpty()) {
-                    Element subjectCode = new Element("SubjectCode", rootElement.getNamespace("ram"));
+                    Element subjectCode = new Element("SubjectCode", ramNs);
                     subjectCode.setText(bg0001.getBT0021InvoiceNoteSubjectCode(0).getValue());
                     includedNote.addContent(subjectCode);
                 }
 
                 if (!bg0001.getBT0022InvoiceNote().isEmpty()) {
-                    Element content = new Element("Content", rootElement.getNamespace("ram"));
+                    Element content = new Element("Content", ramNs);
                     content.setText(bg0001.getBT0022InvoiceNote(0).getValue());
                     includedNote.addContent(content);
                 }
