4161b204955920950eb08ec3bf586d68ab009b71
Matteo Joliveau <m.joliveau@koor.it>
2018-02-14 13:06:17 +0000

    Fix to LogSupport

--- eigor-cli/src/integration-test/java/it/infocert/eigor/cli/ITEigorTest.java
+++ eigor-cli/src/integration-test/java/it/infocert/eigor/cli/ITEigorTest.java
@@ -106,12 +106,12 @@ public class ITEigorTest {
 
         // then
         List<File> files = asList( outputDir.listFiles() );
-        assertThat( "converted invoice, cen invoice, rule report, log expected, got: " + files, files, hasSize(6) );
+        assertThat( "converted invoice, cen invoice, rule report, log expected, got: " + files, files, hasSize(7) );
 
         assertThat( files + " found", findFirstFileByNameOrNull(outputDir, "invoice-cen.csv"), notNullValue() );
         assertThat( files + " found", findFirstFileByNameOrNull(outputDir,"invoice-target.fake"), notNullValue() );
         assertThat( files + " found", findFirstFileByNameOrNull(outputDir, "rule-report.csv"), notNullValue() );
-//        assertThat( files + " found", findFirstFileByNameOrNull(outputDir, "invoice-transformation.log"), notNullValue() );
+        assertThat( files + " found", findFirstFileByNameOrNull(outputDir, "invoice-transformation.log"), notNullValue() );
         assertThat( files + " found", findFirstFileByNameOrNull(outputDir, "invoice-source.xml"), notNullValue() );
 
     }
