c1c085d09b6ad5fecd4fce7e282ab476c57e5272
Matteo Joliveau <matteojoliveau@gmail.com>
2018-04-11 15:22:12 +0200

    Added peppol values to CEN-CII

--- converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/DocumentContextConverter.java
+++ converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/DocumentContextConverter.java
@@ -1,10 +1,15 @@
 package it.infocert.eigor.converter.cen2cii;
 
-import it.infocert.eigor.api.*;
+import it.infocert.eigor.api.CustomConverterUtils;
+import it.infocert.eigor.api.CustomMapping;
+import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.conversion.converter.JavaLocalDateToStringConverter;
 import it.infocert.eigor.api.conversion.converter.TypeConverter;
 import it.infocert.eigor.api.errors.ErrorCode;
-import it.infocert.eigor.model.core.model.*;
+import it.infocert.eigor.model.core.model.BG0000Invoice;
+import it.infocert.eigor.model.core.model.BG0002ProcessControl;
+import it.infocert.eigor.model.core.model.BT0023BusinessProcessType;
+import it.infocert.eigor.model.core.model.BT0024SpecificationIdentifier;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
@@ -42,7 +47,7 @@ public class DocumentContextConverter extends CustomConverterUtils implements Cu
                     BT0023BusinessProcessType bt0023 = bg0002.getBT0023BusinessProcessType(0);
                     id.setText(bt0023.getValue());
                 } else {
-                    id.setText("MISSING_ID");
+                    id.setText("urn:fdc:peppol.eu:2017:poacc:billing:01:1.0");
                 }
                 businessProcessSpecifiedDocumentContextParameter.addContent(id);
                 exchangedDocumentContext.addContent(businessProcessSpecifiedDocumentContextParameter);
@@ -54,7 +59,9 @@ public class DocumentContextConverter extends CustomConverterUtils implements Cu
                     BT0024SpecificationIdentifier bt0024 = bg0002.getBT0024SpecificationIdentifier(0);
                     id.setText(bt0024.getValue());
                 } else {
-                    id.setText("urn:cen.eu:en16931:2017");
+//                    id.setText("urn:cen.eu:en16931:2017");
+//                    PEPPOL hardcoding
+                    id.setText("urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0");
                 }
                 guidelineSpecifiedDocumentContextParameter.addContent(id);
                 exchangedDocumentContext.addContent(guidelineSpecifiedDocumentContextParameter);
