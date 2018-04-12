7b68f0ac4a9f9c8d0bd6bf80ec9ea50f09d5cfae
Matteo Joliveau <matteojoliveau@gmail.com>
2018-04-12 17:55:01 +0200

    Fixed IBAN sometimes not being mapped in UBL-CEN

--- converter-commons/src/main/java/it/infocert/eigor/converter/commons/ubl2cen/CreditTransferConverter.java
+++ converter-commons/src/main/java/it/infocert/eigor/converter/commons/ubl2cen/CreditTransferConverter.java
@@ -9,6 +9,8 @@ import it.infocert.eigor.model.core.model.*;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -16,50 +18,53 @@ import java.util.List;
  * The Credit Transfer Custom Converter
  */
 public class CreditTransferConverter extends CustomConverterUtils implements CustomMapping<Document> {
+    private final static Logger logger = LoggerFactory.getLogger(CreditTransferConverter.class);
 
     public ConversionResult<BG0000Invoice> toBG0017(Document document, BG0000Invoice invoice, List<IConversionIssue> errors) {
 
-        BG0017CreditTransfer bg0017;
+        final Element rootElement = document.getRootElement();
+        final List<Namespace> namespacesInScope = rootElement.getNamespacesIntroduced();
 
-        Element rootElement = document.getRootElement();
-        List<Namespace> namespacesInScope = rootElement.getNamespacesIntroduced();
-
-        List<Element> payeeFinancialAccount;
-        Element paymentMeans = findNamespaceChild(rootElement, namespacesInScope, "PaymentMeans");
+        final Element paymentMeans = findNamespaceChild(rootElement, namespacesInScope, "PaymentMeans");
 
         if (paymentMeans != null) {
-            payeeFinancialAccount = findNamespaceChildren(paymentMeans, namespacesInScope, "PayeeFinancialAccount");
+            final List<Element> payeeFinancialAccount = findNamespaceChildren(paymentMeans, namespacesInScope, "PayeeFinancialAccount");
 
             for (Element elemPayee : payeeFinancialAccount) {
-                	
-            	bg0017 = new BG0017CreditTransfer();
 
-            	Element id = findNamespaceChild(elemPayee, namespacesInScope, "ID");
-                    
-            	if (id != null) {
-            		BT0084PaymentAccountIdentifier bt0084 = new BT0084PaymentAccountIdentifier(id.getText());
-            		bg0017.getBT0084PaymentAccountIdentifier().add(bt0084);
-            	}
-                    
-            	Element name = findNamespaceChild(elemPayee, namespacesInScope, "Name");
-            	if (name != null) {
-            		BT0085PaymentAccountName bt0085 = new BT0085PaymentAccountName(name.getText());
-            		bg0017.getBT0085PaymentAccountName().add(bt0085);
-            	}
-                    
-            	Element financialInstitutionBranch = findNamespaceChild(elemPayee, namespacesInScope, "FinancialInstitutionBranch");
-            	if (financialInstitutionBranch != null) {
-            		Element idBranch = findNamespaceChild(financialInstitutionBranch, namespacesInScope, "ID");
-                    	
-            		if (idBranch != null) {
-            			BT0086PaymentServiceProviderIdentifier bt0086 = new BT0086PaymentServiceProviderIdentifier(idBranch.getText());
-						bg0017.getBT0086PaymentServiceProviderIdentifier().add(bt0086);
-            		}
-            	}
+                final BG0017CreditTransfer bg0017 = new BG0017CreditTransfer();
 
-                if (!invoice.getBG0016PaymentInstructions().isEmpty()) {
-					invoice.getBG0016PaymentInstructions(0).getBG0017CreditTransfer().add(bg0017);
-				}
+                final Element id = findNamespaceChild(elemPayee, namespacesInScope, "ID");
+
+                if (id != null) {
+                    final BT0084PaymentAccountIdentifier bt0084 = new BT0084PaymentAccountIdentifier(id.getText());
+                    bg0017.getBT0084PaymentAccountIdentifier().add(bt0084);
+                    logger.debug("Mapped PayeeFinancialAccount/ID to BT-84 with value: {}", id.getText());
+                } else {
+                    logger.debug("No PayeeFinancialAccount/ID found");
+                }
+
+                final Element name = findNamespaceChild(elemPayee, namespacesInScope, "Name");
+                if (name != null) {
+                    final BT0085PaymentAccountName bt0085 = new BT0085PaymentAccountName(name.getText());
+                    bg0017.getBT0085PaymentAccountName().add(bt0085);
+                }
+
+                final Element financialInstitutionBranch = findNamespaceChild(elemPayee, namespacesInScope, "FinancialInstitutionBranch");
+                if (financialInstitutionBranch != null) {
+                    final Element idBranch = findNamespaceChild(financialInstitutionBranch, namespacesInScope, "ID");
+
+                    if (idBranch != null) {
+                        final BT0086PaymentServiceProviderIdentifier bt0086 = new BT0086PaymentServiceProviderIdentifier(idBranch.getText());
+                        bg0017.getBT0086PaymentServiceProviderIdentifier().add(bt0086);
+                    }
+                }
+
+                if (invoice.getBG0016PaymentInstructions().isEmpty()) {
+                    invoice.getBG0016PaymentInstructions().add(new BG0016PaymentInstructions());
+                }
+
+                invoice.getBG0016PaymentInstructions(0).getBG0017CreditTransfer().add(bg0017);
             }
         }
 
