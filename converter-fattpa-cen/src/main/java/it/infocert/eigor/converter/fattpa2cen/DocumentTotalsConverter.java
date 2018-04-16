c98820604cddbe84a71dd3aea9f2b6c2d90ca8fa
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-04-16 11:49:50 +0300

    fattpa2cen: map DatiRitenuta

--- converter-fattpa-cen/src/main/java/it/infocert/eigor/converter/fattpa2cen/DocumentTotalsConverter.java
+++ converter-fattpa-cen/src/main/java/it/infocert/eigor/converter/fattpa2cen/DocumentTotalsConverter.java
@@ -28,6 +28,8 @@ public class DocumentTotalsConverter implements CustomMapping<Document> {
     }
 
     private void addInvoiceTotalAmountWithVatDefault(BG0000Invoice invoice, Document document, List<IConversionIssue> errors, ErrorCode.Location callingLocation) {
+        TypeConverter<String, Double> strDblConverter = StringToDoubleConverter.newConverter();
+
         Element rootElement = document.getRootElement();
         Element fatturaElettronicaBody = rootElement.getChild("FatturaElettronicaBody");
 
@@ -77,7 +79,6 @@ public class DocumentTotalsConverter implements CustomMapping<Document> {
                                     BT0104DocumentLevelChargeReason bt0104 = new BT0104DocumentLevelChargeReason(bolloVirtualeText);
                                     bg0021.getBT0104DocumentLevelChargeReason().add(bt0104);
                                 } else if (importoBollo != null) {
-                                    TypeConverter<String, Double> strDblConverter = StringToDoubleConverter.newConverter();
 
                                     BT0099DocumentLevelChargeAmount bt0099 = new BT0099DocumentLevelChargeAmount(0d);
                                     bg0021.getBT0099DocumentLevelChargeAmount().add(bt0099);
@@ -102,6 +103,75 @@ public class DocumentTotalsConverter implements CustomMapping<Document> {
                                     }
                                 }
                             }
+                        }
+
+                        Element datiRitenuta = datiGeneraliDocumento.getChild("DatiRitenuta");
+                        if (datiRitenuta != null) {
+                            BG0021DocumentLevelCharges bg0021 = new BG0021DocumentLevelCharges();
+                            invoice.getBG0021DocumentLevelCharges().add(bg0021);
+
+                            BT0099DocumentLevelChargeAmount bt0099 = new BT0099DocumentLevelChargeAmount(0d);
+                            bg0021.getBT0099DocumentLevelChargeAmount().add(bt0099);
+
+                            Element importoRitenuta = datiRitenuta.getChild("ImportoRitenuta");
+                            if (importoRitenuta != null) {
+                                try {
+                                    Double importoRitenutaValue = strDblConverter.convert(importoRitenuta.getText());
+
+                                    BT0100DocumentLevelChargeBaseAmount bt0100 = new BT0100DocumentLevelChargeBaseAmount(importoRitenutaValue);
+                                    bg0021.getBT0100DocumentLevelChargeBaseAmount().add(bt0100);
+
+                                    BT0113PaidAmount bt0113 = new BT0113PaidAmount(importoRitenutaValue);
+                                    invoice.getBG0022DocumentTotals(0).getBT0113PaidAmount().add(bt0113);
+                                } catch (NumberFormatException | ConversionFailedException e) {
+                                    EigorRuntimeException ere = new EigorRuntimeException(e, ErrorMessage.builder().message(e.getMessage())
+                                            .location(callingLocation)
+                                            .action(ErrorCode.Action.HARDCODED_MAP)
+                                            .error(ErrorCode.Error.ILLEGAL_VALUE)
+                                            .addParam(ErrorMessage.SOURCEMSG_PARAM, e.getMessage())
+                                            .build());
+                                    errors.add(ConversionIssue.newError(ere));
+                                }
+                            }
+
+                            Element aliquotaRitenuta = datiRitenuta.getChild("AliquotaRitenuta");
+                            if (aliquotaRitenuta != null) {
+                                BT0101DocumentLevelChargePercentage bt0101 = null;
+                                try {
+                                    bt0101 = new BT0101DocumentLevelChargePercentage(strDblConverter.convert(aliquotaRitenuta.getText()));
+                                } catch (NumberFormatException | ConversionFailedException e) {
+                                    EigorRuntimeException ere = new EigorRuntimeException(e, ErrorMessage.builder().message(e.getMessage())
+                                            .location(callingLocation)
+                                            .action(ErrorCode.Action.HARDCODED_MAP)
+                                            .error(ErrorCode.Error.ILLEGAL_VALUE)
+                                            .addParam(ErrorMessage.SOURCEMSG_PARAM, e.getMessage())
+                                            .build());
+                                    errors.add(ConversionIssue.newError(ere));
+                                }
+                                bg0021.getBT0101DocumentLevelChargePercentage().add(bt0101);
+                            }
+
+                            BT0102DocumentLevelChargeVatCategoryCode bt0102 = new BT0102DocumentLevelChargeVatCategoryCode(Untdid5305DutyTaxFeeCategories.E);
+                            bg0021.getBT0102DocumentLevelChargeVatCategoryCode().add(bt0102);
+
+                            Element tipoRitenuta = datiRitenuta.getChild("TipoRitenuta");
+                            Element causalePagamento = datiRitenuta.getChild("CausalePagamento");
+                            BT0104DocumentLevelChargeReason bt0104 = null;
+                            if (tipoRitenuta != null) {
+                                if (causalePagamento != null) {
+                                    bt0104 = new BT0104DocumentLevelChargeReason(String.format("%s %s", tipoRitenuta.getText(), causalePagamento.getText()));
+                                } else {
+                                    bt0104 = new BT0104DocumentLevelChargeReason(tipoRitenuta.getText());
+                                }
+                            } else if (causalePagamento != null) {
+                                bt0104 = new BT0104DocumentLevelChargeReason(causalePagamento.getText());
+                            }
+                            if (bt0104 != null) {
+                                bg0021.getBT0104DocumentLevelChargeReason().add(bt0104);
+                            }
+
+                            BT0020PaymentTerms bt0020 = new BT0020PaymentTerms("BT-113 represents Withholding tax amount");
+                            invoice.getBT0020PaymentTerms().add(bt0020);
                         }
                     }
                 }
