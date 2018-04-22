dcf7297dd8bbef3181f129ea180e620f5022bb52
Matteo Joliveau <m.joliveau@koor.it>
2018-04-22 10:33:32 +0200

    Fixed BT0046 conversion and added test

--- converter-commons/src/test/java/it/infocert/eigor/converter/commons/cen2ubl/BuyerConverterTest.java
+++ converter-commons/src/test/java/it/infocert/eigor/converter/commons/cen2ubl/BuyerConverterTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.assertEquals;
 public class BuyerConverterTest {
 
     private BG0000Invoice invoice;
+    private BuyerConverter sut;
+    private Document doc;
 
     @Before
     public void setUp() throws Exception {
@@ -26,12 +28,13 @@ public class BuyerConverterTest {
         bg0007.getBT0044BuyerName().add(new BT0044BuyerName("BT-44"));
 
         bg0007.getBT0048BuyerVatIdentifier().add(new BT0048BuyerVatIdentifier(new Identifier("BT-48")));
+
+        this.sut = new BuyerConverter();
+        this.doc = new Document(new Element("Invoice"));
     }
 
     @Test
     public void shouldMapBT0044ToRegistrationName() throws Exception {
-        BuyerConverter sut = new BuyerConverter();
-        Document doc = new Document(new Element("Invoice"));
         sut.map(invoice, doc, Lists.<IConversionIssue>newArrayList(), ErrorCode.Location.UBL_OUT);
 
         Element name = doc.getRootElement().getChild("AccountingCustomerParty").getChild("Party").getChild("PartyLegalEntity").getChild("RegistrationName");
@@ -41,8 +44,6 @@ public class BuyerConverterTest {
 
     @Test
     public void shouldMapBT0056ToRegistrationNameIgnoringBT0044() throws Exception {
-        BuyerConverter sut = new BuyerConverter();
-        Document doc = new Document(new Element("Invoice"));
 
         enrichBG0007BuyerwithBG0009(invoice.getBG0007Buyer(0));
 
@@ -53,10 +54,28 @@ public class BuyerConverterTest {
         assertEquals("BT-56", name.getText());
     }
 
+    @Test
+    public void shouldMapBT0046ConcatenatingSchemeIdAndIdentifier() {
+
+        enrichBG0007BuyerwithBT0046(invoice.getBG0007Buyer(0));
+
+        sut.map(invoice, doc, Lists.<IConversionIssue>newArrayList(), ErrorCode.Location.UBL_OUT);
+
+        Element id = doc.getRootElement().getChild("AccountingCustomerParty").getChild("Party").getChild("PartyIdentification").getChild("ID");
+
+        assertEquals("Test:Schema:Identifier", id.getText());
+    }
+
     private void enrichBG0007BuyerwithBG0009(BG0007Buyer bg0007) {
         BG0009BuyerContact bg0009 = new BG0009BuyerContact();
         bg0007.getBG0009BuyerContact().add(bg0009);
         bg0009.getBT0056BuyerContactPoint().add(new BT0056BuyerContactPoint("BT-56"));
+    }
+
+    private void enrichBG0007BuyerwithBT0046(BG0007Buyer bg0007) {
+        BT0046BuyerIdentifierAndSchemeIdentifier identifier = new BT0046BuyerIdentifierAndSchemeIdentifier(new Identifier("Test:Schema", "Identifier"));
+        bg0007.getBT0046BuyerIdentifierAndSchemeIdentifier().add(identifier);
+
     }
 
 }
