97c059a89c85ac55127032563dc530b347bce618
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-04-24 13:02:32 +0300

    attachment mapping BT-125

--- converter-fattpa-cen/src/main/java/it/infocert/eigor/converter/fattpa2cen/AdditionalSupportingDocumentsConverter.java
+++ converter-fattpa-cen/src/main/java/it/infocert/eigor/converter/fattpa2cen/AdditionalSupportingDocumentsConverter.java
@@ -1,13 +1,18 @@
 package it.infocert.eigor.converter.fattpa2cen;
 
-import it.infocert.eigor.api.ConversionResult;
-import it.infocert.eigor.api.CustomMapping;
-import it.infocert.eigor.api.IConversionIssue;
+import it.infocert.eigor.api.*;
+import it.infocert.eigor.api.configuration.DefaultEigorConfigurationLoader;
+import it.infocert.eigor.api.conversion.ConversionFailedException;
+import it.infocert.eigor.api.conversion.converter.AttachmentToFileReferenceConverter;
+import it.infocert.eigor.api.conversion.converter.TypeConverter;
 import it.infocert.eigor.api.errors.ErrorCode;
+import it.infocert.eigor.api.errors.ErrorMessage;
+import it.infocert.eigor.model.core.datatypes.FileReference;
 import it.infocert.eigor.model.core.model.*;
 import org.jdom2.Document;
 import org.jdom2.Element;
 
+import javax.activation.MimeType;
 import java.util.List;
 
 /**
@@ -39,8 +44,31 @@ public class AdditionalSupportingDocumentsConverter implements CustomMapping<Doc
                     }
                     Element attachment = allegato.getChild("Attachment");
                     if (attachment != null) {
-                        BT0124ExternalDocumentLocation externalDocumentLocation = new BT0124ExternalDocumentLocation(attachment.getText());
-                        bg0024.getBT0124ExternalDocumentLocation().add(externalDocumentLocation);
+                        TypeConverter<Element, FileReference> strToBinConverter = AttachmentToFileReferenceConverter.newConverter(DefaultEigorConfigurationLoader.configuration(), ErrorCode.Location.FATTPA_IN);
+                        try {
+                            Element formatoAttachment = allegato.getChild("FormatoAttachment");
+                            if (formatoAttachment != null) {
+                                attachment.setAttribute("mimeCode", getFullMimeNameFromShortFormat(formatoAttachment.getText()));
+                            }
+                            if (nomeAttachment != null) {
+                                attachment.setAttribute("filename", nomeAttachment.getText());
+                            }
+                            BT0125AttachedDocumentAndAttachedDocumentMimeCodeAndAttachedDocumentFilename bt0125 =
+                                    new BT0125AttachedDocumentAndAttachedDocumentMimeCodeAndAttachedDocumentFilename(strToBinConverter.convert(attachment));
+                            bg0024.getBT0125AttachedDocumentAndAttachedDocumentMimeCodeAndAttachedDocumentFilename().add(bt0125);
+                        } catch (IllegalArgumentException | ConversionFailedException e) {
+                            EigorRuntimeException ere = new EigorRuntimeException(
+                                    e,
+                                    ErrorMessage.builder()
+                                            .message(e.getMessage())
+                                            .location(ErrorCode.Location.FATTPA_IN)
+                                            .action(ErrorCode.Action.HARDCODED_MAP)
+                                            .error(ErrorCode.Error.ILLEGAL_VALUE)
+                                            .addParam(ErrorMessage.SOURCEMSG_PARAM, e.getMessage())
+                                            .addParam(ErrorMessage.OFFENDINGITEM_PARAM, attachment.toString())
+                                            .build());
+                            errors.add(ConversionIssue.newError(ere));
+                        }
                     }
                     invoice.getBG0024AdditionalSupportingDocuments().add(bg0024);
                 }
@@ -53,5 +81,25 @@ public class AdditionalSupportingDocumentsConverter implements CustomMapping<Doc
     @Override
     public void map(BG0000Invoice cenInvoice, Document document, List<IConversionIssue> errors, ErrorCode.Location callingLocation) {
         toBG0024(document, cenInvoice, errors);
+    }
+
+    private String getFullMimeNameFromShortFormat(String shortFormat) {
+        if (shortFormat != null) {
+            switch (shortFormat.toLowerCase()) {
+                case "pdf":
+                    return "application/pdf";
+                case "ods":
+                    return "application/vnd.oasis.opendocument.spreadsheet";
+                case "xlsx":
+                    return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                case "png":
+                    return "image/png";
+                case "jpeg":
+                    return "image/jpeg";
+                case "csv":
+                    return "text/csv";
+            }
+        }
+        return null;
     }
 }
