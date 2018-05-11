04da9da9afaaa4ee6b54877c1ee6a798e7ffa33d
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-05-11 11:21:06 +0300

    cen2ubl: fix BT-22 Note cardinality + tests

--- converter-commons/src/main/java/it/infocert/eigor/converter/commons/cen2ubl/NoteConverter.java
+++ converter-commons/src/main/java/it/infocert/eigor/converter/commons/cen2ubl/NoteConverter.java
@@ -5,6 +5,7 @@ import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.errors.ErrorCode;
 import it.infocert.eigor.model.core.model.BG0000Invoice;
 import it.infocert.eigor.model.core.model.BG0001InvoiceNote;
+import it.infocert.eigor.model.core.model.BT0022InvoiceNote;
 import org.jdom2.Document;
 import org.jdom2.Element;
 
@@ -19,17 +20,15 @@ public class NoteConverter extends FirstLevelElementsConverter {
 
         if (!invoice.getBG0001InvoiceNote().isEmpty()) {
             for (BG0001InvoiceNote bg0001 : invoice.getBG0001InvoiceNote()) {
-                if (!bg0001.getBT0022InvoiceNote().isEmpty()) {
+                for (BT0022InvoiceNote bt0022 : bg0001.getBT0022InvoiceNote()) {
                     Element note = new Element("Note");
-                    String bt0022 = bg0001.getBT0022InvoiceNote(0).getValue();
 
                     if (bg0001.getBT0021InvoiceNoteSubjectCode().isEmpty()) {
-                        note.setText(bt0022);
+                        note.setText(bt0022.getValue());
                     } else {
                         String bt0021 = bg0001.getBT0021InvoiceNoteSubjectCode(0).getValue();
-                        note.setText("#" + bt0021 + "#" + bt0022);
+                        note.setText("#" + bt0021 + "#" + bt0022.getValue());
                     }
-
                     root.addContent(note);
                 }
             }
--- converter-commons/src/test/java/it/infocert/eigor/converter/commons/cen2ubl/FirstLevelElementsConverterTest.java
+++ converter-commons/src/test/java/it/infocert/eigor/converter/commons/cen2ubl/FirstLevelElementsConverterTest.java
@@ -11,6 +11,7 @@ import org.jdom2.Element;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,46 +20,45 @@ public class FirstLevelElementsConverterTest {
 
     @Test
     public void InvoiceNoteShouldHavePrependedSubjectCodeIfBT0021(){
-        BG0000Invoice invoice = createInvoiceWithBG0001InvoiceNoteAndBT0022(true);
+        BG0000Invoice invoice = createInvoiceWithBG0001InvoiceNoteAndBT0022s(true);
         Document document = new Document(new Element("Invoice"));
 
         NoteConverter converter = new NoteConverter();
         converter.map(invoice, document, new ArrayList<IConversionIssue>(), ErrorCode.Location.UBL_OUT);
 
-        Element note = document.getRootElement().getChild("Note");
-        String noteText = note.getText();
+        List<Element> notes = document.getRootElement().getChildren("Note");
 
-        assertEquals("#TESTSUBJECTCODE#TESTNOTE", noteText);
+        assertEquals("#TESTSUBJECTCODE#TESTNOTE1", notes.get(0).getValue());
+        assertEquals("#TESTSUBJECTCODE#TESTNOTE2", notes.get(1).getValue());
     }
 
     @Test
     public void InvoiceNoteShouldNotHavePrependedSubjectCodeWithoutBT0021(){
-        BG0000Invoice invoice = createInvoiceWithBG0001InvoiceNoteAndBT0022(false);
+        BG0000Invoice invoice = createInvoiceWithBG0001InvoiceNoteAndBT0022s(false);
         Document document = new Document(new Element("Invoice"));
 
         NoteConverter converter = new NoteConverter();
 
         converter.map(invoice, document, new ArrayList<IConversionIssue>(), ErrorCode.Location.UBL_OUT);
 
-        Element note = document.getRootElement().getChild("Note");
-        String noteText = note.getText();
+        List<Element> notes = document.getRootElement().getChildren("Note");
 
-        assertEquals("TESTNOTE", noteText);
+        assertEquals("TESTNOTE1", notes.get(0).getValue());
+        assertEquals("TESTNOTE2", notes.get(1).getValue());
     }
 
-    private BG0000Invoice createInvoiceWithBG0001InvoiceNoteAndBT0022(boolean withBT0021InvoiceNoteSubjectCode) {
+    private BG0000Invoice createInvoiceWithBG0001InvoiceNoteAndBT0022s(boolean withBT0021InvoiceNoteSubjectCode) {
         BG0000Invoice invoice = new BG0000Invoice();
 
         BG0001InvoiceNote bg0001 = new BG0001InvoiceNote();
-        BT0022InvoiceNote bt0022 = new BT0022InvoiceNote("TESTNOTE");
 
         if (withBT0021InvoiceNoteSubjectCode) {
             BT0021InvoiceNoteSubjectCode bt0021 = new BT0021InvoiceNoteSubjectCode("TESTSUBJECTCODE");
             bg0001.getBT0021InvoiceNoteSubjectCode().add(bt0021);
         }
 
-
-        bg0001.getBT0022InvoiceNote().add(bt0022);
+        bg0001.getBT0022InvoiceNote().add(new BT0022InvoiceNote("TESTNOTE1"));
+        bg0001.getBT0022InvoiceNote().add(new BT0022InvoiceNote("TESTNOTE2"));
         invoice.getBG0001InvoiceNote().add(bg0001);
 
         return invoice;
