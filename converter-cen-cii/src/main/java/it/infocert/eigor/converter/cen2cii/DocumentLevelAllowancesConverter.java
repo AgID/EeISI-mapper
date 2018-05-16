1a9f9b670308ad247105859d00285306d4c06ed7
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-05-16 14:29:31 +0300

    cen2cii: add missing namespaces for allowances/charges/taxparty elements

--- converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/DocumentLevelAllowancesConverter.java
+++ converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/DocumentLevelAllowancesConverter.java
@@ -24,78 +24,81 @@ public class DocumentLevelAllowancesConverter extends CustomConverterUtils imple
 
         Element rootElement = document.getRootElement();
         List<Namespace> namespacesInScope = rootElement.getNamespacesIntroduced();
+        Namespace rsmNs = rootElement.getNamespace("rsm");
+        Namespace ramNs = rootElement.getNamespace("ram");
+        Namespace udtNs = rootElement.getNamespace("udt");
 
         Element supplyChainTradeTransaction = findNamespaceChild(rootElement, namespacesInScope, "SupplyChainTradeTransaction");
         Element applicableHeaderTradeSettlement = null;
 
         if (supplyChainTradeTransaction == null) {
-            supplyChainTradeTransaction = new Element("SupplyChainTradeTransaction", rootElement.getNamespace("rsm"));
-            applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", rootElement.getNamespace("ram"));
+            supplyChainTradeTransaction = new Element("SupplyChainTradeTransaction", rsmNs);
+            applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", ramNs);
             supplyChainTradeTransaction.addContent(applicableHeaderTradeSettlement);
             rootElement.addContent(supplyChainTradeTransaction);
         } else {
             applicableHeaderTradeSettlement = findNamespaceChild(supplyChainTradeTransaction, namespacesInScope, "ApplicableHeaderTradeSettlement");
             if (applicableHeaderTradeSettlement == null) {
-                applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", rootElement.getNamespace("ram"));
+                applicableHeaderTradeSettlement = new Element("ApplicableHeaderTradeSettlement", ramNs);
                 supplyChainTradeTransaction.addContent(applicableHeaderTradeSettlement);
             }
         }
 
         for (BG0020DocumentLevelAllowances bg0020 : cenInvoice.getBG0020DocumentLevelAllowances()) {
-            Element specifiedTradeAllowanceCharge = new Element("SpecifiedTradeAllowanceCharge", rootElement.getNamespace("ram"));
+            Element specifiedTradeAllowanceCharge = new Element("SpecifiedTradeAllowanceCharge", ramNs);
 
-            Element chargeIndicator = new Element("ChargeIndicator", rootElement.getNamespace("ram"));
-            Element indicator = new Element("Indicator", rootElement.getNamespace("ram"));
+            Element chargeIndicator = new Element("ChargeIndicator", ramNs);
+            Element indicator = new Element("Indicator", udtNs);
             indicator.setText("false");
             chargeIndicator.addContent(indicator);
             specifiedTradeAllowanceCharge.addContent(chargeIndicator);
 
             if (!bg0020.getBT0094DocumentLevelAllowancePercentage().isEmpty()) {
-                Element calculationPercent = new Element("CalculationPercent");
+                Element calculationPercent = new Element("CalculationPercent", ramNs);
                 BigDecimal value = bg0020.getBT0094DocumentLevelAllowancePercentage(0).getValue();
                 calculationPercent.setText(value.setScale(2, RoundingMode.HALF_UP).toString());
                 specifiedTradeAllowanceCharge.addContent(calculationPercent);
             }
 
             if (!bg0020.getBT0092DocumentLevelAllowanceAmount().isEmpty()) {
-                Element actualAmount = new Element("ActualAmount");
+                Element actualAmount = new Element("ActualAmount", ramNs);
                 BigDecimal value = bg0020.getBT0092DocumentLevelAllowanceAmount(0).getValue();
                 actualAmount.setText(value.setScale(2, RoundingMode.HALF_UP).toString());
                 specifiedTradeAllowanceCharge.addContent(actualAmount);
             }
 
             if (!bg0020.getBT0093DocumentLevelAllowanceBaseAmount().isEmpty()) {
-                Element basisAmount = new Element("BasisAmount");
+                Element basisAmount = new Element("BasisAmount", ramNs);
                 BigDecimal value = bg0020.getBT0093DocumentLevelAllowanceBaseAmount(0).getValue();
                 basisAmount.setText(value.setScale(2, RoundingMode.HALF_UP).toString());
                 specifiedTradeAllowanceCharge.addContent(basisAmount);
             }
 
 
-            Element categoryTradeTax = new Element("CategoryTradeTax");
+            Element categoryTradeTax = new Element("CategoryTradeTax", ramNs);
             specifiedTradeAllowanceCharge.addContent(categoryTradeTax);
 
             if (!bg0020.getBT0095DocumentLevelAllowanceVatCategoryCode().isEmpty()) {
-                Element categoryCode = new Element("CategoryCode");
+                Element categoryCode = new Element("CategoryCode", ramNs);
                 categoryCode.setText(bg0020.getBT0095DocumentLevelAllowanceVatCategoryCode(0).getValue().name());
                 categoryTradeTax.addContent(categoryCode);
             }
 
             if (!bg0020.getBT0096DocumentLevelAllowanceVatRate().isEmpty()) {
-                Element rateApplicablePercent = new Element("RateApplicablePercent");
+                Element rateApplicablePercent = new Element("RateApplicablePercent", ramNs);
                 BigDecimal value = bg0020.getBT0096DocumentLevelAllowanceVatRate(0).getValue();
                 rateApplicablePercent.setText(value.setScale(2, RoundingMode.HALF_UP).toString());
                 categoryTradeTax.addContent(rateApplicablePercent);
             }
 
             if (!bg0020.getBT0097DocumentLevelAllowanceReason().isEmpty()) {
-                Element reason = new Element("Reason");
+                Element reason = new Element("Reason", ramNs);
                 reason.setText(bg0020.getBT0097DocumentLevelAllowanceReason(0).getValue());
                 specifiedTradeAllowanceCharge.addContent(reason);
             }
 
             if (!bg0020.getBT0098DocumentLevelAllowanceReasonCode().isEmpty()) {
-                Element reasonCode = new Element("ReasonCode");
+                Element reasonCode = new Element("ReasonCode", ramNs);
                 reasonCode.setText(bg0020.getBT0098DocumentLevelAllowanceReasonCode(0).getValue().name());
                 specifiedTradeAllowanceCharge.addContent(reasonCode);
             }
