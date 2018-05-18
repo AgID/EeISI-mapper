2ebe62d3fbfa70fc5a1c196cd67c56f44795a862
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-05-18 11:23:06 +0300

    cen2fattpa enforce decimals

--- converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/jaxbindings/Amount8DecimalAdapter.java
+++ converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/jaxbindings/Amount8DecimalAdapter.java
@@ -3,10 +3,10 @@ package it.infocert.eigor.converter.cen2fattpa.jaxbindings;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
-public class RateAdapter {
+public class Amount8DecimalAdapter {
 
     public static String marshal(BigDecimal v) {
-        return new DecimalFormat("#0.00").format(v);
+        return new DecimalFormat("#0.00000000").format(v);
     }
 
     public static BigDecimal unmarshal(String v) {
