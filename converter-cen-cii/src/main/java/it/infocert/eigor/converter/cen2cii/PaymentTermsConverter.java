f8256626f9db17c0c29be3e1dd387af28697b0b3
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-05-25 14:14:12 +0300

    cen2cii: fix multiple schematron assert fails about document totals

--- converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/PaymentTermsConverter.java
+++ converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/PaymentTermsConverter.java
@@ -7,6 +7,7 @@ import it.infocert.eigor.api.conversion.converter.TypeConverter;
 import it.infocert.eigor.api.errors.ErrorCode;
 import it.infocert.eigor.model.core.enums.Iso4217CurrenciesFundsCodes;
 import it.infocert.eigor.model.core.model.*;
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
@@ -105,85 +106,131 @@ public class PaymentTermsConverter extends CustomConverterUtils implements Custo
                 applicableHeaderTradeSettlement.addContent(specifiedTradeSettlementHeaderMonetarySummation);
             }
 
-            if (!bg0022.getBT0106SumOfInvoiceLineNetAmount().isEmpty()) {
-                BigDecimal bt0106 = bg0022.getBT0106SumOfInvoiceLineNetAmount(0).getValue();
-                Element lineTotalAmount = new Element("LineTotalAmount", ramNs);
-                lineTotalAmount.setText(bt0106.setScale(2, RoundingMode.HALF_UP).toString());
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(lineTotalAmount);
+            String currencyId = null;
+            if (!invoice.getBT0005InvoiceCurrencyCode().isEmpty()) {
+                currencyId = invoice.getBT0005InvoiceCurrencyCode(0).getValue().name();
             }
 
-            if (!bg0022.getBT0108SumOfChargesOnDocumentLevel().isEmpty()) {
-                BigDecimal bt0108 = bg0022.getBT0108SumOfChargesOnDocumentLevel(0).getValue();
+            final BigDecimal bt106SumOfInvoiceLineNetAmounts = getBT106SumOfInvoiceLineNetAmounts(invoice);
+            final BigDecimal bt107SumOfAllowances = getBT107SumOfAllowances(invoice);
+            final BigDecimal bt108SumOfCharges = getBT108SumOfCharges(invoice);
+            final BigDecimal bt109TaxExclusiveAmount = bt106SumOfInvoiceLineNetAmounts.subtract(bt107SumOfAllowances).add(bt108SumOfCharges);
+            final BigDecimal bt110TotalTaxAmount = getBT110TotalTaxAmount(invoice);
+            final BigDecimal bt112TaxInclusiveAmount = bt109TaxExclusiveAmount.add(bt110TotalTaxAmount);
+            final BigDecimal bt0113PrepaidAmount = bg0022.getBT0113PaidAmount().isEmpty() ? BigDecimal.ZERO : bg0022.getBT0113PaidAmount(0).getValue();
+            final BigDecimal bt0114RoundingAmount = bg0022.getBT0114RoundingAmount().isEmpty() ? BigDecimal.ZERO : bg0022.getBT0114RoundingAmount(0).getValue();
+            final BigDecimal bt0115PayableAmount = bt112TaxInclusiveAmount.subtract(bt0113PrepaidAmount).add(bt0114RoundingAmount);
+
+
+//          <xsd:element name="LineTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element lineTotalAmount = new Element("LineTotalAmount", ramNs);
+            lineTotalAmount.setText(bt106SumOfInvoiceLineNetAmounts.setScale(2, RoundingMode.HALF_UP).toString());
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(lineTotalAmount);
+
+//			<xsd:element name="ChargeTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            if (BigDecimal.ZERO.compareTo(bt108SumOfCharges) != 0) {
                 Element chargeTotalAmount = new Element("ChargeTotalAmount", ramNs);
-                chargeTotalAmount.setText(bt0108.setScale(2, RoundingMode.HALF_UP).toString());
+                chargeTotalAmount.setText(bt108SumOfCharges.setScale(2, RoundingMode.HALF_UP).toString());
                 specifiedTradeSettlementHeaderMonetarySummation.addContent(chargeTotalAmount);
             }
 
-            if (!bg0022.getBT0107SumOfAllowancesOnDocumentLevel().isEmpty()) {
-                BigDecimal bt0107 = bg0022.getBT0107SumOfAllowancesOnDocumentLevel(0).getValue();
+//			<xsd:element name="AllowanceTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            if (BigDecimal.ZERO.compareTo(bt107SumOfAllowances) != 0) {
                 Element allowanceTotalAmount = new Element("AllowanceTotalAmount", ramNs);
-                allowanceTotalAmount.setText(bt0107.setScale(2, RoundingMode.HALF_UP).toString());
+                allowanceTotalAmount.setText(bt107SumOfAllowances.setScale(2, RoundingMode.HALF_UP).toString());
                 specifiedTradeSettlementHeaderMonetarySummation.addContent(allowanceTotalAmount);
             }
 
-            if (!bg0022.getBT0109InvoiceTotalAmountWithoutVat().isEmpty()) {
-                BigDecimal bt0109 = bg0022.getBT0109InvoiceTotalAmountWithoutVat(0).getValue();
-                Element taxBasisTotalAmount = new Element("TaxBasisTotalAmount", ramNs);
-                taxBasisTotalAmount.setText(bt0109.setScale(2, RoundingMode.HALF_UP).toString());
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(taxBasisTotalAmount);
-            }
+//			<xsd:element name="TaxBasisTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element taxBasisTotalAmount = new Element("TaxBasisTotalAmount", ramNs);
+            taxBasisTotalAmount.setText(bt109TaxExclusiveAmount.setScale(2, RoundingMode.HALF_UP).toString());
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(taxBasisTotalAmount);
 
-            Element taxTotalAmount = taxTotalAmount(errors, callingLocation, ramNs, bg0022, invoice);
-            if (taxTotalAmount != null) {
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(taxTotalAmount);
+//			<xsd:element name="TaxTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element taxTotalAmount = new Element("TaxTotalAmount", ramNs);
+            taxTotalAmount.setText(bt110TotalTaxAmount.setScale(2, RoundingMode.HALF_UP).toString());
+            if(currencyId != null){
+                taxTotalAmount.setAttribute("currencyID", currencyId);
             }
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(taxTotalAmount);
 
-            //FIXME BT-111
+//			<xsd:element name="RoundingAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element roundingAmount = new Element("RoundingAmount", ramNs);
+            roundingAmount.setText(bt0114RoundingAmount.setScale(2, RoundingMode.HALF_UP).toString());
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(roundingAmount);
 
-            if (!bg0022.getBT0114RoundingAmount().isEmpty()) {
-                BigDecimal bt0114 = bg0022.getBT0114RoundingAmount(0).getValue();
-                Element roundingAmount = new Element("RoundingAmount", ramNs);
-                roundingAmount.setText(bt0114.setScale(2, RoundingMode.HALF_UP).toString());
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(roundingAmount);
-            }
+//			<xsd:element name="GrandTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element grandTotalAmount = new Element("GrandTotalAmount", ramNs);
+            grandTotalAmount.setText(bt112TaxInclusiveAmount.setScale(2, RoundingMode.HALF_UP).toString());
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(grandTotalAmount);
 
-            if (!bg0022.getBT0112InvoiceTotalAmountWithVat().isEmpty()) {
-                BigDecimal bt0112 = bg0022.getBT0112InvoiceTotalAmountWithVat(0).getValue();
-                Element grandTotalAmount = new Element("GrandTotalAmount", ramNs);
-                grandTotalAmount.setText(bt0112.setScale(2, RoundingMode.HALF_UP).toString());
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(grandTotalAmount);
-            }
+//			<xsd:element name="InformationAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
 
-            if (!bg0022.getBT0113PaidAmount().isEmpty()) {
-                BigDecimal bt0113 = bg0022.getBT0113PaidAmount(0).getValue();
-                Element totalPrepaidAmount = new Element("TotalPrepaidAmount", ramNs);
-                totalPrepaidAmount.setText(bt0113.setScale(2, RoundingMode.HALF_UP).toString());
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(totalPrepaidAmount);
-            }
+//			<xsd:element name="TotalPrepaidAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element totalPrepaidAmount = new Element("TotalPrepaidAmount", ramNs);
+            totalPrepaidAmount.setText(bt0113PrepaidAmount.setScale(2, RoundingMode.HALF_UP).toString());
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(totalPrepaidAmount);
 
-            if (!bg0022.getBT0115AmountDueForPayment().isEmpty()) {
-                BigDecimal bt0115 = bg0022.getBT0115AmountDueForPayment(0).getValue();
-                Element duePayableAmount = new Element("DuePayableAmount", ramNs);
-                duePayableAmount.setText(bt0115.setScale(2, RoundingMode.HALF_UP).toString());
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(duePayableAmount);
-            }
+//			<xsd:element name="TotalDiscountAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="TotalAllowanceChargeAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+
+//			<xsd:element name="DuePayableAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+            Element duePayableAmount = new Element("DuePayableAmount", ramNs);
+            duePayableAmount.setText(bt0115PayableAmount.setScale(2, RoundingMode.HALF_UP).toString());
+            specifiedTradeSettlementHeaderMonetarySummation.addContent(duePayableAmount);
+
+//			<xsd:element name="RetailValueExcludingTaxInformationAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="TotalDepositFeeInformationAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="ProductValueExcludingTobaccoTaxInformationAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="TotalRetailValueInformationAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="GrossLineTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="NetLineTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
+//			<xsd:element name="NetIncludingTaxesLineTotalAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
         }
     }
 
-    private Element taxTotalAmount(List<IConversionIssue> errors, ErrorCode.Location callingLocation, Namespace ramNs, BG0022DocumentTotals bg0022, BG0000Invoice invoice) {
-
-        Element taxTotalAmount = null;
-        if (!bg0022.getBT0110InvoiceTotalVatAmount().isEmpty()) {
-            BigDecimal bt0110 = bg0022.getBT0110InvoiceTotalVatAmount(0).getValue();
-            taxTotalAmount = new Element("TaxTotalAmount", ramNs);
-            taxTotalAmount.setText(bt0110.setScale(2, RoundingMode.HALF_UP).toString());
-
-            if (!invoice.getBT0005InvoiceCurrencyCode().isEmpty()) {
-                Iso4217CurrenciesFundsCodes bt0005 = invoice.getBT0005InvoiceCurrencyCode(0).getValue();
-                taxTotalAmount.setAttribute("currencyID", bt0005.getCode());
+    private BigDecimal getBT106SumOfInvoiceLineNetAmounts(BG0000Invoice invoice) {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (BG0025InvoiceLine bg0025 : invoice.getBG0025InvoiceLine()) {
+            if (!bg0025.getBT0131InvoiceLineNetAmount().isEmpty()) {
+                BT0131InvoiceLineNetAmount bt0131 = bg0025.getBT0131InvoiceLineNetAmount(0);
+                sum = sum.add(bt0131.getValue());
             }
         }
-        return taxTotalAmount;
+        return sum;
+    }
+
+    private BigDecimal getBT107SumOfAllowances(BG0000Invoice invoice) {
+        BigDecimal sumOfAllowances = BigDecimal.ZERO;
+        for (BG0020DocumentLevelAllowances bg0020 : invoice.getBG0020DocumentLevelAllowances()) {
+            if (!bg0020.getBT0092DocumentLevelAllowanceAmount().isEmpty()) {
+                BT0092DocumentLevelAllowanceAmount bt0092 = bg0020.getBT0092DocumentLevelAllowanceAmount(0);
+                sumOfAllowances = sumOfAllowances.add(bt0092.getValue());
+            }
+        }
+        return sumOfAllowances;
+    }
+
+    private BigDecimal getBT108SumOfCharges(BG0000Invoice invoice) {
+        BigDecimal sumOfCharges = BigDecimal.ZERO;
+        for (BG0021DocumentLevelCharges bg0021 : invoice.getBG0021DocumentLevelCharges()) {
+            if (!bg0021.getBT0099DocumentLevelChargeAmount().isEmpty()) {
+                BT0099DocumentLevelChargeAmount bt0099 = bg0021.getBT0099DocumentLevelChargeAmount(0);
+                sumOfCharges = sumOfCharges.add(bt0099.getValue());
+            }
+        }
+        return sumOfCharges;
+    }
+
+    private BigDecimal getBT110TotalTaxAmount(BG0000Invoice cenInvoice) {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (BG0023VatBreakdown bg0023 : cenInvoice.getBG0023VatBreakdown()) {
+            if (!bg0023.getBT0117VatCategoryTaxAmount().isEmpty()) {
+                BT0117VatCategoryTaxAmount bt0117 = bg0023.getBT0117VatCategoryTaxAmount(0);
+                sum = sum.add(bt0117.getValue());
+            }
+        }
+        return sum;
     }
 }
 
