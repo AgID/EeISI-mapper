add77bfbe5099a250c29d027ca201fc0ec9fb98e
Alexandru Enasoae <a.enasoae@techgapsolutions.com>
2018-06-08 23:32:22 +0300

    fix #288

--- converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/jaxbindings/Amount2DecimalAdapter.java
+++ converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/jaxbindings/Amount2DecimalAdapter.java
@@ -2,11 +2,13 @@ package it.infocert.eigor.converter.cen2fattpa.jaxbindings;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class Amount2DecimalAdapter {
 
     public static String marshal(BigDecimal v) {
-        return new DecimalFormat("#0.00").format(v);
+        return new DecimalFormat("#0.00", DecimalFormatSymbols.getInstance(Locale.US)).format(v);
     }
 
     public static BigDecimal unmarshal(String v) {
--- converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/jaxbindings/Amount8DecimalAdapter.java
+++ converter-cen-fattpa/src/main/java/it/infocert/eigor/converter/cen2fattpa/jaxbindings/Amount8DecimalAdapter.java
@@ -2,11 +2,13 @@ package it.infocert.eigor.converter.cen2fattpa.jaxbindings;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class Amount8DecimalAdapter {
 
     public static String marshal(BigDecimal v) {
-        return new DecimalFormat("#0.00000000").format(v);
+        return new DecimalFormat("#0.00000000", DecimalFormatSymbols.getInstance(Locale.US)).format(v);
     }
 
     public static BigDecimal unmarshal(String v) {
