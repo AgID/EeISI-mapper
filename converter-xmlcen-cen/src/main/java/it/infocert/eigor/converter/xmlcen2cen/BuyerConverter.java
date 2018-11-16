5c85ba1358864e50bf2c59334de3a52b9f32fcaf
Alessandro Arici <a.arici@techgap.it>
2018-11-16 17:23:44 +0100

    Eisi 15 - xmlcen to cen, centralizing bt conversion

--- converter-xmlcen-cen/src/main/java/it/infocert/eigor/converter/xmlcen2cen/BuyerConverter.java
+++ converter-xmlcen-cen/src/main/java/it/infocert/eigor/converter/xmlcen2cen/BuyerConverter.java
@@ -4,18 +4,18 @@ import it.infocert.eigor.api.ConversionResult;
 import it.infocert.eigor.api.CustomMapping;
 import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.errors.ErrorCode;
-import it.infocert.eigor.model.core.datatypes.Identifier;
 import it.infocert.eigor.model.core.model.BG0000Invoice;
 import it.infocert.eigor.model.core.model.BG0007Buyer;
 import it.infocert.eigor.model.core.model.BT0046BuyerIdentifierAndSchemeIdentifier;
 import it.infocert.eigor.model.core.model.BT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier;
 import it.infocert.eigor.model.core.model.BT0049BuyerElectronicAddressAndSchemeIdentifier;
-import org.codehaus.plexus.util.StringUtils;
+import it.infocert.eigor.model.core.model.BTBG;
 import org.jdom2.Document;
 import org.jdom2.Element;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class BuyerConverter implements CustomMapping<Document> {
 
@@ -32,38 +32,26 @@ public class BuyerConverter implements CustomMapping<Document> {
 
             final List<Element> bt46s = rootElement.getChild("BG-7").getChildren("BT-46");
             bt46s.forEach(bt46 -> {
-                final BT0046BuyerIdentifierAndSchemeIdentifier buyerIdentifierAndSchemeIdentifier;
-                if(Objects.nonNull(bt46.getAttribute("scheme")) && StringUtils.isNotEmpty(bt46.getAttribute("scheme").getValue())) {
-                    final String scheme = bt46.getAttribute("scheme").getValue();
-                    buyerIdentifierAndSchemeIdentifier = new BT0046BuyerIdentifierAndSchemeIdentifier(new Identifier(scheme, bt46.getText()));
-                } else {
-                    buyerIdentifierAndSchemeIdentifier = new BT0046BuyerIdentifierAndSchemeIdentifier(new Identifier(bt46.getText()));
+                final Optional<BTBG> bt = ConverterUtils.getBt.apply("BT0046", bt46);
+                if(bt.isPresent()) {
+                    buyer.getBT0046BuyerIdentifierAndSchemeIdentifier().add((BT0046BuyerIdentifierAndSchemeIdentifier) bt.get());
                 }
-                buyer.getBT0046BuyerIdentifierAndSchemeIdentifier().add(buyerIdentifierAndSchemeIdentifier);
             });
 
             final List<Element> bt47s = rootElement.getChild("BG-7").getChildren("BT-47");
             bt47s.forEach(bt47 -> {
-                final BT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier buyerLegalRegistrationIdentifierAndSchemeIdentifier;
-                if(Objects.nonNull(bt47.getAttribute("scheme")) && StringUtils.isNotEmpty(bt47.getAttribute("scheme").getValue())) {
-                    final String scheme = bt47.getAttribute("scheme").getValue();
-                    buyerLegalRegistrationIdentifierAndSchemeIdentifier = new BT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier(new Identifier(scheme, bt47.getText()));
-                } else {
-                    buyerLegalRegistrationIdentifierAndSchemeIdentifier = new BT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier(new Identifier(bt47.getText()));
+                final Optional<BTBG> bt = ConverterUtils.getBt.apply("BT0047", bt47);
+                if(bt.isPresent()) {
+                    buyer.getBT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier().add((BT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier) bt.get());
                 }
-                buyer.getBT0047BuyerLegalRegistrationIdentifierAndSchemeIdentifier().add(buyerLegalRegistrationIdentifierAndSchemeIdentifier);
             });
 
             final List<Element> bt49s = rootElement.getChild("BG-7").getChildren("BT-49");
             bt49s.forEach(bt49 -> {
-                final BT0049BuyerElectronicAddressAndSchemeIdentifier buyerElectronicAddressAndSchemeIdentifier;
-                if(Objects.nonNull(bt49.getAttribute("scheme")) && StringUtils.isNotEmpty(bt49.getAttribute("scheme").getValue())) {
-                    final String scheme = bt49.getAttribute("scheme").getValue();
-                    buyerElectronicAddressAndSchemeIdentifier = new BT0049BuyerElectronicAddressAndSchemeIdentifier(new Identifier(scheme, bt49.getText()));
-                } else {
-                    buyerElectronicAddressAndSchemeIdentifier = new BT0049BuyerElectronicAddressAndSchemeIdentifier(new Identifier(bt49.getText()));
+                final Optional<BTBG> bt = ConverterUtils.getBt.apply("BT0049", bt49);
+                if(bt.isPresent()) {
+                    buyer.getBT0049BuyerElectronicAddressAndSchemeIdentifier().add((BT0049BuyerElectronicAddressAndSchemeIdentifier) bt.get());
                 }
-                buyer.getBT0049BuyerElectronicAddressAndSchemeIdentifier().add(buyerElectronicAddressAndSchemeIdentifier);
             });
         }
 
--- converter-xmlcen-cen/src/main/java/it/infocert/eigor/converter/xmlcen2cen/DeliveryInformationConverter.java
+++ converter-xmlcen-cen/src/main/java/it/infocert/eigor/converter/xmlcen2cen/DeliveryInformationConverter.java
@@ -4,20 +4,20 @@ import it.infocert.eigor.api.ConversionResult;
 import it.infocert.eigor.api.CustomMapping;
 import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.errors.ErrorCode;
-import it.infocert.eigor.model.core.datatypes.Identifier;
 import it.infocert.eigor.model.core.model.BG0000Invoice;
 import it.infocert.eigor.model.core.model.BG0013DeliveryInformation;
 import it.infocert.eigor.model.core.model.BT0071DeliverToLocationIdentifierAndSchemeIdentifier;
-import org.codehaus.plexus.util.StringUtils;
+import it.infocert.eigor.model.core.model.BTBG;
 import org.jdom2.Document;
 import org.jdom2.Element;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class DeliveryInformationConverter implements CustomMapping<Document> {
 
-    public ConversionResult<BG0000Invoice> toBG0004(Document document, BG0000Invoice invoice, List<IConversionIssue> errors) {
+    public ConversionResult<BG0000Invoice> toBG0013(Document document, BG0000Invoice invoice, List<IConversionIssue> errors) {
 
         Element rootElement = document.getRootElement();
         Element bg13 = rootElement.getChild("BG-13");
@@ -26,18 +26,14 @@ public class DeliveryInformationConverter implements CustomMapping<Document> {
             if (invoice.getBG0013DeliveryInformation().isEmpty()) {
                 invoice.getBG0013DeliveryInformation().add(new BG0013DeliveryInformation());
             }
-            final BG0013DeliveryInformation payee = invoice.getBG0013DeliveryInformation().get(0);
+            final BG0013DeliveryInformation deliveryInformation = invoice.getBG0013DeliveryInformation().get(0);
             
             final List<Element> bt71s = rootElement.getChild("BG-13").getChildren("BT-71");
             bt71s.forEach(bt71 -> {
-                final BT0071DeliverToLocationIdentifierAndSchemeIdentifier deliverToLocationIdentifierAndSchemeIdentifier;
-                if(Objects.nonNull(bt71.getAttribute("scheme")) && StringUtils.isNotEmpty(bt71.getAttribute("scheme").getValue())) {
-                    final String scheme = bt71.getAttribute("scheme").getValue();
-                    deliverToLocationIdentifierAndSchemeIdentifier = new BT0071DeliverToLocationIdentifierAndSchemeIdentifier(new Identifier(scheme, bt71.getText()));
-                } else {
-                    deliverToLocationIdentifierAndSchemeIdentifier = new BT0071DeliverToLocationIdentifierAndSchemeIdentifier(new Identifier(bt71.getText()));
+                final Optional<BTBG> bt = ConverterUtils.getBt.apply("BT0071", bt71);
+                if(bt.isPresent()) {
+                    deliveryInformation.getBT0071DeliverToLocationIdentifierAndSchemeIdentifier().add((BT0071DeliverToLocationIdentifierAndSchemeIdentifier) bt.get());
                 }
-                payee.getBT0071DeliverToLocationIdentifierAndSchemeIdentifier().add(deliverToLocationIdentifierAndSchemeIdentifier);
             });
         }
 
@@ -46,6 +42,6 @@ public class DeliveryInformationConverter implements CustomMapping<Document> {
 
     @Override
     public void map(BG0000Invoice cenInvoice, Document document, List<IConversionIssue> errors, ErrorCode.Location callingLocation) {
-        toBG0004(document, cenInvoice, errors);
+        toBG0013(document, cenInvoice, errors);
     }
 }
