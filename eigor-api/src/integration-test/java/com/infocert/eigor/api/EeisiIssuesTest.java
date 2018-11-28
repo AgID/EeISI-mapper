15ebc0392dae32120fc95455630aca80f787cedb
Daniele Demichelis <d.demichelis@techgap.it>
2018-11-28 18:02:25 +0100

    The trimming test works.

--- eigor-api/src/integration-test/java/com/infocert/eigor/api/EeisiIssuesTest.java
+++ eigor-api/src/integration-test/java/com/infocert/eigor/api/EeisiIssuesTest.java
@@ -4,7 +4,10 @@ import com.infocert.eigor.api.ConversionUtil.KeepAll;
 import it.infocert.eigor.api.ConversionResult;
 import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.configuration.ConfigurationException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.IOUtils;
+import org.hamcrest.Matchers;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 import org.w3c.dom.Document;
@@ -27,8 +30,10 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.*;
 import java.util.Base64;
+import java.util.List;
 
 import static it.infocert.eigor.test.Utils.invoiceAsStream;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -76,30 +81,31 @@ public class EeisiIssuesTest {
      * <pre>
      *     &lt;cac:PartyIdentification&gt;&lt;cbc:ID&gt;IT:ALBO:IngegneriElettroniciInformaticiIngegneriElettroniciInformatici:123456789012345678901234567890123456789012345678901234567890111&lt;/cbc:ID
      * </pre>
-     * This is too long to be stored in XML PA, hence, the untruncated value should be stored in "attachment".
+     * This is too long to be stored in XML PA, hence, the not truncated value should be stored in an attached file, in CSV format.
+     * This CSV should have several columns, one for the untruncated value, the other for the truncated value.
      */
     @Test
-    public void issueEeisi22() throws XPathExpressionException {
+    public void issueEeisi22() throws XPathExpressionException, IOException {
+
+        // a conversion UBL - fatturaPA withouth errors.
+        ConversionResult<byte[]> conversion = this.conversion.assertConversionWithoutErrors("/issues/issue-eeisi22-ubl.xml", "ubl", "fatturapa");
+
+        // The CSV in base 64 is the 3rd attachment in this case.
+        String truncatedValuesCSVInBase64 = evalXpathExpression(conversion, "//*[local-name()='Allegati'][3]/*[local-name()='Attachment']/text()");
+
+        String csvSource = new String(Base64.getDecoder().decode(truncatedValuesCSVInBase64));
 
 
-        ConversionResult<byte[]> convert = conversion.assertConversionWithoutErrors("/issues/issue-eeisi22-ubl.xml", "ubl", "fatturapa");
+        List<CSVRecord> records = CSVFormat
+                .DEFAULT
+                .withFirstRecordAsHeader()
+                .parse( new StringReader(csvSource) ).getRecords();
+        assertThat( records.size(), is(2) );
+        assertThat( records.get(0).get("Original value"), is("IngegneriElettroniciInformaticiIngegneriElettroniciInformatici") );
+        assertThat( records.get(0).get("Trimmed value"), is("IngegneriElettroniciInformaticiIngegneriElettroniciInformati") );
+        assertThat( records.get(1).get("Original value"), is("123456789012345678901234567890123456789012345678901234567890111") );
+        assertThat( records.get(1).get("Trimmed value"), is("123456789012345678901234567890123456789012345678901234567890") );
 
-        System.out.println( new String(convert.getResult()) );
-
-        String s = evalXpathExpression(convert, "//*[local-name()='Allegati'][3]/*[local-name()='Attachment']/text()");
-
-        System.out.println( s );
-
-        String s1 = new String(Base64.getDecoder().decode(s));
-
-        System.out.println( s1 );
-
-//        String evaluate = evalXpathExpression(convert, "//*[local-name()='FatturaElettronicaBody']//*[local-name()='DatiGenerali']//*[local-name()='DatiTrasporto']//*[local-name()='DataOraConsegna']/text()");
-//
-//        assertTrue(convert.getIssues().isEmpty()); // no warnings for text exceeding length limit
-//
-//        assertTrue(evaluate != null && !evaluate.trim().isEmpty());
-//        Assert.assertEquals(conversion.buildMsgForFailedAssertion(convert, new KeepAll(), null), "2017-10-15T00:00:00", evaluate);
     }
 
     private String evalXpathExpression(ConversionResult<byte[]> convert, String expression) throws XPathExpressionException {
