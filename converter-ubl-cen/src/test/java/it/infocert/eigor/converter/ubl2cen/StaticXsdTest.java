482ffddf04e7f63dda244bf1a51fd27c7286607a
Daniele Demichelis <d.demichelis@techgap.it>
2019-01-19 12:46:01 +0100

    Test speed improvements

--- converter-ubl-cen/src/test/java/it/infocert/eigor/converter/ubl2cen/StaticXsdTest.java
+++ converter-ubl-cen/src/test/java/it/infocert/eigor/converter/ubl2cen/StaticXsdTest.java
@@ -4,6 +4,7 @@ import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.errors.ErrorCode;
 import it.infocert.eigor.api.xml.XSDValidator;
 import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -15,17 +16,22 @@ import static org.junit.Assert.assertThat;
 
 public class StaticXsdTest {
 
+    XSDValidator sut;
+
+    @Before
+    public void setUp() throws SAXException {
+        sut = new XSDValidator(FileUtils.getFile("../converter-commons/src/main/resources/converterdata/converter-commons/ubl/xsdstatic/UBL-Invoice-2.1.xsd"), ErrorCode.Location.UBL_IN);
+    }
+
     @Test
     public void shoudLoadTheOnlineXsdVersion() throws SAXException {
-        XSDValidator xsdValidator = new XSDValidator(FileUtils.getFile("../converter-commons/src/main/resources/converterdata/converter-commons/ubl/xsd/UBL-Invoice-2.1.xsd"), ErrorCode.Location.UBL_IN);
-        List<IConversionIssue> issues = xsdValidator.validate("<xml>bad xml</xml>".getBytes());
+        List<IConversionIssue> issues = sut.validate("<xml>bad xml</xml>".getBytes());
         assertThat(issues, not(empty()));
     }
 
     @Test
     public void shoudLoadTheStaticXsdVersion() throws SAXException {
-        XSDValidator xsdValidator = new XSDValidator(FileUtils.getFile("../converter-commons/src/main/resources/converterdata/converter-commons/ubl/xsd/UBL-Invoice-2.1.xsd"), ErrorCode.Location.UBL_IN);
-        List<IConversionIssue> issues = xsdValidator.validate("<xml>bad xml</xml>".getBytes());
+        List<IConversionIssue> issues = sut.validate("<xml>bad xml</xml>".getBytes());
         assertThat(issues, not(empty()));
     }
 
