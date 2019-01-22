35e4164693af5a8727eb6232e8695c94bcc060d5
Daniele Demichelis <d.demichelis@techgap.it>
2019-01-22 18:05:14 +0100

    BT3 now converted with a mapper.

--- converter-cen-fattpa/src/test/java/it/infocert/eigor/converter/cen2fattpa/Cen2FattPATest.java
+++ converter-cen-fattpa/src/test/java/it/infocert/eigor/converter/cen2fattpa/Cen2FattPATest.java
@@ -6,8 +6,10 @@ import it.infocert.eigor.api.configuration.ConfigurationException;
 import it.infocert.eigor.api.configuration.DefaultEigorConfigurationLoader;
 import it.infocert.eigor.api.configuration.EigorConfiguration;
 import it.infocert.eigor.api.utils.JavaReflections;
+import it.infocert.eigor.converter.cen2ubl.ConverterUnitTest;
 import it.infocert.eigor.model.core.enums.Iso31661CountryCodes;
 import it.infocert.eigor.model.core.enums.UnitOfMeasureCodes;
+import it.infocert.eigor.model.core.enums.Untdid1001InvoiceTypeCode;
 import it.infocert.eigor.model.core.model.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +19,6 @@ import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.*;
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
@@ -27,18 +28,44 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.*;
 
-public class Cen2FattPATest {
+public class Cen2FattPATest extends ConverterUnitTest {
     private static final Logger log = LoggerFactory.getLogger(Cen2FattPATest.class);
 
     private Cen2FattPA converter;
-    private XPathFactory xPathFactory;
 
     @Before
     public void setUp() throws ConfigurationException {
         EigorConfiguration conf = new DefaultEigorConfigurationLoader().loadConfiguration();
         converter = new Cen2FattPA(new JavaReflections(), conf);
         converter.configure();
-        xPathFactory = XPathFactory.newInstance();
+    }
+
+    @Test
+    public void eisi26Bt3ToTipoDocumentShouldBeMapped() throws Exception{
+
+        // given
+        BG0000Invoice invoice = new BG0000Invoice();
+        invoice
+                .getBT0003InvoiceTypeCode()
+                .add(new BT0003InvoiceTypeCode(Untdid1001InvoiceTypeCode.Code380));
+
+
+        // when
+        byte[] fattpaXML = converter.convert(invoice).getResult();
+
+        // then
+        Document dom = factory.newDocumentBuilder().parse(new ByteArrayInputStream(fattpaXML));
+
+        String stringByXPath = getStringByXPath(dom,
+                "/*[local-name()='FatturaElettronica']" +
+                        "/*[local-name()='FatturaElettronicaBody']" +
+                        "/*[local-name()='DatiGenerali']" +
+                        "/*[local-name()='DatiGeneraliDocumento']" +
+                        "/*[local-name()='TipoDocumento']/text()"
+        );
+
+        assertEquals("TD01", stringByXPath);
+
     }
 
     @Test
@@ -111,10 +138,6 @@ public class Cen2FattPATest {
         return invoice;
     }
 
-    private String getStringByXPath(Document doc, String xpath) throws XPathExpressionException {
-        XPath xPath = xPathFactory.newXPath();
-        XPathExpression xPathExpression = xPath.compile(xpath);
-        return (String) xPathExpression.evaluate(doc, XPathConstants.STRING);
-    }
+
 
 }
