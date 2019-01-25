b8b54fbe0aaa42fa6a449bb2110103605ec786bf
Daniele Demichelis <d.demichelis@techgap.it>
2019-01-25 16:33:25 +0100

    fix ResultUtils

--- eigor-core/src/main/java/it/infocert/eigor/api/utils/ResultUtils.java
+++ eigor-core/src/main/java/it/infocert/eigor/api/utils/ResultUtils.java
@@ -5,49 +5,59 @@ import it.infocert.eigor.api.IConversionIssue;
 import it.infocert.eigor.api.errors.ErrorMessage;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class ResultUtils {
+/** Various utility methods that alows you to handle {@link it.infocert.eigor.api.ConversionResult results}. */
+public final class ResultUtils {
 
     public static final String toString(ConversionResult<?> result) {
 
-        List<IConversionIssue> issues = result.getIssues();
+        List<IConversionIssue> warns = getWarningIssues(result);
+        List<IConversionIssue> errors = getErrorIssues(result);
 
-        List<IConversionIssue> warns = issues.stream()
-                .filter(issue -> issue.isWarning())
-                .collect(Collectors.toList());
-
-        List<IConversionIssue> errors = issues.stream()
-                .filter(issue -> issue.isError())
-                .collect(Collectors.toList());
 
         StringBuilder sb = new StringBuilder();
-
         sb.append( String.format("%3$d errors, %2$d warnings, result of type '%1$s'\n", result.getResult().getClass().getName(), warns.size(), errors.size()) );
-
         if(!errors.isEmpty()) {
             sb.append("\nerrors:\n");
-            for(int i=0; i<errors.size(); i++) {
-
-                IConversionIssue error = errors.get(i);
-
-                ErrorMessage errorMessage = error.getErrorMessage();
-
-                sb.append(String.format("%d) %s\n", i+1, errorMessage.getErrorCode()));
-                sb.append(String.format("%s\n\n", errorMessage.getMessage()));
-
-            }
+            sb.append(prettyPrintIssues(errors));
         }
-
         if(!warns.isEmpty()) {
             sb.append("\nwarnings:\n");
-            for (IConversionIssue warn : warns) {
-                sb.append(String.format("%s\n", warn));
-            }
+            sb.append(prettyPrintIssues(warns));
         }
-
         return sb.toString();
 
+    }
+
+    public static String prettyPrintIssues(List<IConversionIssue> issues) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<issues.size(); i++) {
+            IConversionIssue error = issues.get(i);
+            ErrorMessage errorMessage = error.getErrorMessage();
+            sb.append(String.format("%d) %s\n", i+1, errorMessage.getErrorCode()));
+            sb.append(String.format("%s\n\n", errorMessage.getMessage()));
+        }
+        return sb.toString();
+    }
+
+    public static List<IConversionIssue> getWarningIssues(ConversionResult<?> result) {
+        return findIssues(result, issue -> issue.isWarning());
+    }
+
+    public static List<IConversionIssue> getErrorIssues(ConversionResult<?> result) {
+        return findIssues(result, issue -> issue.isError());
+    }
+
+    public static List<IConversionIssue> findIssues(ConversionResult<?> result, Predicate<IConversionIssue> iConversionIssuePredicate) {
+        return result.getIssues().stream()
+                    .filter(iConversionIssuePredicate)
+                    .collect(Collectors.toList());
+    }
+
+    private ResultUtils() {
+        throw new UnsupportedOperationException("Should not be instantiated");
     }
 
 }
