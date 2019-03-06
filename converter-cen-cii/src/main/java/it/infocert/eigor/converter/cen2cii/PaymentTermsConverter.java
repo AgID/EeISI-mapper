8a09ab88b34792e7db47a542602f6259ff3dcf01
Alexandru Mocioi <a.mocioi@techgapsolutions.com>
2019-03-07 01:17:39 +0200

    Fix EISI-189

--- converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/PaymentTermsConverter.java
+++ converter-cen-cii/src/main/java/it/infocert/eigor/converter/cen2cii/PaymentTermsConverter.java
@@ -156,14 +156,6 @@ public class PaymentTermsConverter extends CustomConverterUtils implements Custo
             }
             specifiedTradeSettlementHeaderMonetarySummation.addContent(taxTotalAmount);
 
-            if(bt111OrNull!=null && bt0005OrNull!=null) {
-                specifiedTradeSettlementHeaderMonetarySummation.addContent(
-                        new Element("TaxTotalAmount", ramNs)
-                                .setText(bt111OrNull.setScale(2, RoundingMode.HALF_UP).toString())
-                                .setAttribute("currencyID", bt0005OrNull)
-                );
-            }
-
 //			<xsd:element name="RoundingAmount" type="udt:AmountType" minOccurs="0" maxOccurs="unbounded"/>
             Element roundingAmount = new Element("RoundingAmount", ramNs);
             roundingAmount.setText(bt0114RoundingAmount.setScale(2, RoundingMode.HALF_UP).toString());
