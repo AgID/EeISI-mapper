88ccd1abd406bd25cee0079ae7819c66f2da8f9b
Stefano Orlandi <sorlandi@sorint.it>
2019-04-03 18:17:39 +0200

    fix 300 and refactoring

--- converter-commons/src/main/java/it/infocert/eigor/converter/commons/cen2ubl/InvoiceTypeCodeConverter.java
+++ converter-commons/src/main/java/it/infocert/eigor/converter/commons/cen2ubl/InvoiceTypeCodeConverter.java
@@ -9,7 +9,7 @@ import org.jdom2.Document;
 import java.util.List;
 
 public class InvoiceTypeCodeConverter extends FirstLevelElementsConverter {
-    
+
     @Override
     public void customMap(BG0000Invoice invoice, Document document, List<IConversionIssue> errors, ErrorCode.Location callingLocation) {
 
@@ -17,14 +17,12 @@ public class InvoiceTypeCodeConverter extends FirstLevelElementsConverter {
             Untdid1001InvoiceTypeCode bt0003 = invoice.getBT0003InvoiceTypeCode(0).getValue();
             String converted = conversionRegistry.convert(Untdid1001InvoiceTypeCode.class, String.class, bt0003);
 
-            if (ErrorCode.Location.UBL_OUT.equals(callingLocation)) {
+            if (ErrorCode.Location.UBL_OUT.equals(callingLocation) || ErrorCode.Location.PEPPOL_OUT.equals(callingLocation)) {
                 convert("InvoiceTypeCode", converted);
             }
-            if (ErrorCode.Location.UBLCN_OUT.equals(callingLocation)) {
+            if (ErrorCode.Location.UBLCN_OUT.equals(callingLocation) || ErrorCode.Location.PEPPOLCN_OUT.equals(callingLocation)) {
                 convert("CreditNoteTypeCode", converted);
             }
         }
-
     }
-
 }
